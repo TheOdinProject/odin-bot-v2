b601658335003ea4b6ea7a98bf2f2fc4b5da791d
Cody Loyd <codyloyd@gmail.com>
2019-12-27 13:21:14 -0600

    add rudimentary rate limiting.

--- botEngine.js
+++ botEngine.js
@@ -1,12 +1,30 @@
 const botCommands = [];
 
+let authorBuffer = []
+
+const createAuthorEntry = function(message) {
+  const entry = {
+    author: message.author.id,
+    timeOut: false
+  }
+
+  setTimeout(function(){
+    entry.timeOut = true
+  }, 60000)
+
+  return entry
+}
+
+const flushAuthorEntries = function() {
+  authorBuffer = authorBuffer.filter(entry => entry.timeOut == false)
+}
+
 function registerBotCommand(regex, fn) {
   botCommands.push({ regex, fn });
 }
 
 async function listenToMessages(client) {
   client.on("message", message => {
-    console.log(message)
     // Prevent bot from responding to its own messages
     if (message.author === client.user) {
       return;
@@ -25,8 +43,23 @@ async function listenToMessages(client) {
       return;
     }
 
+    const authorEntryCount = authorBuffer.reduce((count, current) => {
+      if (current.author == message.author.id) {
+        return count + 1
+      }
+    },0)
+
+    flushAuthorEntries()
+
+    if (authorEntryCount > 10) {
+      console.log('DENIED')
+      return
+    }
+
+
     botCommands.forEach(async ({ regex, fn }) => {
       if (message.content.toLowerCase().match(regex)) {
+        authorBuffer.push(createAuthorEntry(message))
         try {
           const response = await fn(message);
 
