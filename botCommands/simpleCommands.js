da7b79704d0e8891913fd6045e241106ea4e4691
Ryan Ford <ryanford@email.com>
2020-04-18 12:05:05 +0800

    fix all the regex start bounds

--- botCommands/simpleCommands.js
+++ botCommands/simpleCommands.js
@@ -1,10 +1,10 @@
 const {registerBotCommand} = require('../botEngine.js');
 
-registerBotCommand(/\/hug/, () => `⊂(´・ω・｀⊂)`);
+registerBotCommand(/(?<=\s?)\/hug/, () => `⊂(´・ω・｀⊂)`);
 
-registerBotCommand(/\/smart/, () => String.raw`f(ಠ‿↼)z`);
+registerBotCommand(/(?<=\s?)\/smart/, () => String.raw`f(ಠ‿↼)z`);
 
-registerBotCommand(/\/lenny/, () => String.raw`( ͡° ͜ʖ ͡°)`);
+registerBotCommand(/(?<=\s?)\/lenny/, () => String.raw`( ͡° ͜ʖ ͡°)`);
 
 registerBotCommand(/:fu:/, async (message) => {
   await message.reply('http://media.riffsy.com/images/636a97aa416ad674eb2b72d4a6e9ad6c/tenor.gif')
@@ -13,7 +13,7 @@ registerBotCommand(/:fu:/, async (message) => {
 
 registerBotCommand(/(?<=\s?)\/question\b/, () => `**It looks like you're trying to ask a question! Please give this page a read: https://medium.com/@gordon_zhu/how-to-be-great-at-asking-questions-e37be04d0603**`);
 
-registerBotCommand(/\/data/, () => `**Please state your question in the form of a question! https://www.dontasktoask.com/**`);
+registerBotCommand(/(?<=\s?)\/data/, () => `**Please state your question in the form of a question! https://www.dontasktoask.com/**`);
 
 registerBotCommand(/:fu:/, ({ data }) => {
   const user = data.fromUser.username;
@@ -28,7 +28,7 @@ registerBotCommand(/\peen/, ({author}) => {
   }
 });
 
-registerBotCommand(/\B\/google\s+.+/, ({content}) => {
+registerBotCommand(/(?<=\s?)\/google\s+.+/, ({content}) => {
   const transform = content => {
     const query = content.split(' ').map(encodeURIComponent).join('+');
     return `**HERE YOU GO BABY >** <https://lmgtfy.com/?q=${query}>`;
@@ -37,7 +37,7 @@ registerBotCommand(/\B\/google\s+.+/, ({content}) => {
   const query = content.match(/\B\/google\s+(.+)/)[1];
   return `${transform(query)}`;
 });
-registerBotCommand(/\B\/fg\s+.+/, ({content}) => {
+registerBotCommand(/(?<=\s?)\/fg\s+.+/, ({content}) => {
   const transform = content => {
     const query = content.split(' ').map(encodeURIComponent).join('+');
     return `**This is what you should have typed into Google >** <https://google.com/search?q=${query}>`;
@@ -48,15 +48,15 @@ registerBotCommand(/\B\/fg\s+.+/, ({content}) => {
 });
 
 
-registerBotCommand(/\/dab/, () => `https://tenor.com/view/bettywhite-dab-gif-5044603`);
+registerBotCommand(/(?<=\s?)\/dab/, () => `https://tenor.com/view/bettywhite-dab-gif-5044603`);
 
 registerBotCommand(
-  /\/gandalf/,
+  /(?<=\s?)\/gandalf/,
   () => `http://emojis.slackmojis.com/emojis/images/1450458362/181/gandalf.gif`
 );
 
 
-registerBotCommand(/\/motivate/, () => {
+registerBotCommand(/(?<=\s?)\/motivate/, () => {
   return `Don't give up! https://www.youtube.com/watch?v=KxGRhd_iWuE`;
 });
 
