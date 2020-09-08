758f13c67b4e2a66dc81de7493a85ad1ab9472b7
Ryan Ford <ryanford-dev@users.noreply.github.com>
2020-09-09 00:35:18 +0800

    Add message directing creations discussion to creations-discussion

--- botEngine.js
+++ botEngine.js
@@ -29,7 +29,7 @@ async function listenToMessages(client) {
     if (message.author === client.user) {
       return;
     }
-    
+
     const regex = new RegExp("ok", "i");
     const NOBOT_ROLE_ID = "513916941212188698";
 
@@ -52,12 +52,17 @@ async function listenToMessages(client) {
         {
           message.channel.send(`Hello there, ${message.author}! It seems you misunderstood our instructions. Please return to <#693244715839127653> and read carefully.`);
         }
-      else 
+      else
         {
           message.channel.send(`Hello ${message.author}! If you haven't yet, go read the <#693244715839127653> for instructions on how to access the rest of our discord server.
 If you are still having trouble after following the instructions, DM a Maintainer or Core member.`);
         }
       return;
+    } else if (message.channel.id === '') { // creations-showcase
+      message.channel
+        .send("Reminder: This channel is for posting links to your creations only. You can discuss the projects posted here in the sibling channel #creations-discussion")
+        .delete({ timeout: 43200000 }); // self delete after 12 hours
+			return;
     }
 
     const authorEntryCount = authorBuffer.reduce((count, current) => {
