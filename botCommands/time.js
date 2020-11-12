eb939a1262708d46e6b07716a20f1d95254d3a5b
Tatiana <tatianaw83@gmail.com>
2020-11-12 14:07:02 -0800

    Removed unnecessary parameter and reference array over collection

--- botCommands/time.js
+++ botCommands/time.js
@@ -1,13 +1,12 @@
-const config = require("../config.js");
 const { registerBotCommand } = require("../botEngine.js");
-registerBotCommand(/\B\/time\b/, async ({ room, mentions }) => {
+registerBotCommand(/\B\/time\b/, async ({ mentions }) => {
     let users = '';
     const mentionedUsers = mentions.users.array()
-    if (mentions.users.size >= 3){   
+    if (mentionedUsers.length >= 3){   
         mentionedUsers.forEach((user, index) => {
             return index < mentionedUsers.length - 1 ? users += ` ${user},` : users += ` and ${user}`
         })  
-    } else if (mentions.users.size === 2) {
+    } else if (mentionedUsers.length === 2) {
         mentionedUsers.forEach((user, index) => {
             return index < mentionedUsers.length - 1 ? users += ` ${user}` : users += ` and ${user}`
         })
