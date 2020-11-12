6aa4a1994dd0fa14b41bc626c25f645063ecaf89
Marvin Gay <60161078+marvingay@users.noreply.github.com>
2020-11-12 17:25:03 -0500

    Update time.js

--- botCommands/time.js
+++ botCommands/time.js
@@ -10,7 +10,7 @@ registerBotCommand(/\B\/time\b/, async ({ mentions }) => {
         mentionedUsers.forEach((user, index) => {
             return index < mentionedUsers.length - 1 ? users += ` ${user}` : users += ` and ${user}`
         })
-    } else {
+    } else if (mentionedUsers.length === 1){
         users =  `, ${mentionedUsers[0]}`
     }
     return !users ? `Time is an illusion. Please read this: https://discord.com/channels/505093832157691914/505093832157691916/765633002393829389` : `Time is an illusion${users}. Please read this: https://discord.com/channels/505093832157691914/505093832157691916/765633002393829389`;
