0dba92d7d3406fdf4fbe0b429923e3b3bc6dab63
Tatiana <tatianaw83@gmail.com>
2020-12-01 19:19:23 -0800

    switched tabs to spaces

--- botCommands/mockData.js
+++ botCommands/mockData.js
@@ -1,21 +1,23 @@
 const { Collection, ClientUser } = require("discord.js")
 
 const generateMentions = (number) =>{
-	const collection = new Collection()
+  const collection = new Collection()
+  
   for(let i = 0; i < number; i++){
-	  collection.set(`User${i+1}`,
-			new ClientUser ('client',
-				{
-					id: i + 1,
-						username: `User${i+1}`
-				})
-		)
-	}
-	return {
-		mentions : {
-			users : collection
-		}
-	}
+    collection.set(`User${i+1}`,
+    new ClientUser ('client',
+      {
+      id: i + 1,
+      username: `User${i+1}`
+      })
+    )
+  }
+
+  return {
+    mentions : {
+    users : collection
+    }
+  }
 }
 
 module.exports = generateMentions
