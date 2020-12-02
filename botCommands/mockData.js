dc133580e2311d41ea3fee8f27bb751ff9de8a73
Tatiana <tatianaw83@gmail.com>
2020-12-01 19:13:33 -0800

    switched spacing from 4 to 2, and added tests for mentions helper

--- botCommands/mockData.js
+++ botCommands/mockData.js
@@ -1,24 +1,22 @@
 const { Collection, ClientUser } = require("discord.js")
 
 const generateMentions = (number) =>{
-    const collection = new Collection()
-    for(let i = 0; i < number; i++){
-        if (number === 0) return 
-        collection.set(`User${i+1}`,
-            new ClientUser ('client',
-                {
-                    id: i,
-                    username: `User${i+1}`
-                })
-        )
-    }
-    return {
-        mentions : {
-            users : collection
-        }
-    }
+	const collection = new Collection()
+  for(let i = 0; i < number; i++){
+	  collection.set(`User${i+1}`,
+			new ClientUser ('client',
+				{
+					id: i + 1,
+						username: `User${i+1}`
+				})
+		)
+	}
+	return {
+		mentions : {
+			users : collection
+		}
+	}
 }
- 
 
 module.exports = generateMentions
-  
+
