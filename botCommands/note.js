3d7e9791a6caf4dbfe825d58dc149c14bc26b54a
Daegudude <k3hppk@gmail.com>
2021-01-08 22:16:18 +0900

    Updated note file to the new format

--- botCommands/note.js
+++ botCommands/note.js
@@ -1,8 +1,10 @@
 const { registerBotCommand } = require("../botEngine.js");
 
-const noteDoc =
-  "https://docs.google.com/document/d/1fas8xwMRi4GVrnCtp9U5OL7Y9QB6v5H_Wx19m4UQwgI/edit";
+const command = {
+  regex: /\B\/notes?\b/,
+  cb: () => `To note, or not to note, that is the question. Please read this: https://docs.google.com/document/d/1fas8xwMRi4GVrnCtp9U5OL7Y9QB6v5H_Wx19m4UQwgI/edit`
+}
 
-registerBotCommand(/\B\/notes?\b/, () => {
-  `To note, or not to note, that is the question. Please read this: ${noteDoc}`;
-});
+registerBotCommand(command.regex, command.cb);
+
+module.exports = command;
