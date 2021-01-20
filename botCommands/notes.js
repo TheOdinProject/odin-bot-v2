3bc1654de50d8545e167eef7f629aca48f137bd1
Daegudude <k3hppk@gmail.com>
2021-01-20 22:29:44 +0900

    Updated notes command to pass the all tests

--- botCommands/notes.js
+++ botCommands/notes.js
@@ -1,7 +1,7 @@
 const { registerBotCommand } = require("../botEngine.js");
 
 const command = {
-  regex: /\B\/notes/,
+  regex: /(?<!\S)\/notes(?!\S)/,
   cb: () => { 
     return `To note, or not to note, that is the question. Please read this: https://discord.com/channels/505093832157691914/505093832157691916/768161823366578176`
   },
