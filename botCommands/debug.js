a80332650931fc678801f5b8f8ec1a20c62b93d0
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-02-12 00:11:45 +0530

    Extend /debug command

--- botCommands/debug.js
+++ botCommands/debug.js
@@ -2,8 +2,32 @@ const { registerBotCommand } = require("../botEngine.js");
 
 const command = {
   regex: /(?<!\S)\/debug(?!\S)/,
-  cb: () =>
-    `Looks like this problem can be solved using a debugger: <https://developers.google.com/web/tools/chrome-devtools/javascript>`,
+  cb: ({ content }) => {
+    const query = content.match(/\B\/debug\s?(\S+)?/)[1];
+
+    const template = `Based on the description of your problem, you can get to the root of it using a debugger. Learning how to track down problems like this is an inevitable part of being a developer.`;
+
+    const defaultReply =
+      template +
+      `\n<https://en.wikipedia.org/wiki/Debugging>
+
+To get a helpful resource in javascript or ruby, run \`/debug js\` or \`/debug rb\`.`;
+
+    switch (query) {
+      case "js":
+        return (
+          template +
+          `\n<https://developers.google.com/web/tools/chrome-devtools/javascript>`
+        );
+      case "rb":
+        return (
+          template +
+          `\n<https://www.theodinproject.com/courses/ruby-programming/lessons/debugging#debugging-with-pry-byebug>`
+        );
+      default:
+        return defaultReply;
+    }
+  },
 };
 
 registerBotCommand(command.regex, command.cb);
