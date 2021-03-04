b1e7b8f2171c30eaad938e2a9c89e9c8791b7176
Jonathan Colman <joncolman94@gmail.com>
2021-03-04 12:45:33 +0000

    Lint callbacks.test.js to comply with ESlint
    callbacks.test.js failed ESlint.
    Replaced all unnecessary double quotes with single quotes.

--- botCommands/callbacks.test.js
+++ botCommands/callbacks.test.js
@@ -1,61 +1,60 @@
-/* eslint-disable */
-const command = require("./callbacks");
+const command = require('./callbacks');
 
-describe("/callbacks", () => {
+describe('/callbacks', () => {
   it.each([
-    ["/callbacks"],
-    [" /callbacks"],
-    ["/callbacks @odin-bot"],
-    [" /callbacks @odin-bot"],
+    ['/callbacks'],
+    [' /callbacks'],
+    ['/callbacks @odin-bot'],
+    [' /callbacks @odin-bot'],
   ])("'%s' triggers the callback", (string) => {
     expect(command.regex.test(string)).toBeTruthy();
   });
 
   it.each([
-    ["/coolbacks"],
-    ["callbacks"],
-    ["/cllbacks"],
-    ["/callback"],
-    ["/call backs"],
+    ['/coolbacks'],
+    ['callbacks'],
+    ['/cllbacks'],
+    ['/callback'],
+    ['/call backs'],
     ['```function("/call backs", () => {}```'],
-    ["/CallBacks"],
-    [""],
-    [" "],
-    ["/ "],
-    ["@odin-bot / callbacks"],
-    ["/call&ba^cks"],
-    ["/callba^cks"],
-    ["@odin-bot/ callbacks"],
+    ['/CallBacks'],
+    [''],
+    [' '],
+    ['/ '],
+    ['@odin-bot / callbacks'],
+    ['/call&ba^cks'],
+    ['/callba^cks'],
+    ['@odin-bot/ callbacks'],
   ])("'%s' does not trigger the callback", (string) => {
     expect(command.regex.test(string)).toBeFalsy();
   });
 
   it.each([
-    ["how about dem /callbacks"],
-    ["i think /callbacks are great"],
-    ["hey @odin-bot, teach me /callbacks"],
-    ["/@odin-bot ^ /me /callbacks /test$*"],
+    ['how about dem /callbacks'],
+    ['i think /callbacks are great'],
+    ['hey @odin-bot, teach me /callbacks'],
+    ['/@odin-bot ^ /me /callbacks /test$*'],
   ])("'%s' - the command can be anywhere in the string", (string) => {
     expect(command.regex.test(string)).toBeTruthy();
   });
 
   it.each([
-    ["@user/callbacks"],
-    ["watchaknowabout/callbacks"],
-    ["/callbackssgrealkdmsfalnd"],
-    ["/callbacks/"],
-    ["/callbacks*"],
-    ["/callbacks..."],
+    ['@user/callbacks'],
+    ['watchaknowabout/callbacks'],
+    ['/callbackssgrealkdmsfalnd'],
+    ['/callbacks/'],
+    ['/callbacks*'],
+    ['/callbacks...'],
   ])(
     "'%s' - command should be its own word/group - no leading or trailing characters",
     (string) => {
       expect(command.regex.test(string)).toBeFalsy();
-    }
+    },
   );
 });
 
-describe("/callbacks callback", () => {
-  it("should return the correct output", () => {
+describe('/callbacks callback', () => {
+  it('should return the correct output', () => {
     expect(command.cb()).toMatchSnapshot();
   });
 });
