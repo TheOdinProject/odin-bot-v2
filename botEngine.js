d2a5250a56dd4e61c1a8ecf8f6997d8f42757138
Jonathan Colman <joncolman94@gmail.com>
2021-03-04 17:04:27 +0000

    Lint botEngine.js to comply with ESlint
    Previously, botEngine.js failed ESlint
    
    Place missing semicolons and trailing commas
    Replace '==' with '===' (eqeqeq)
    Replace '!=' with '!==' (eqeqeq)
    Place parentheses around arrow function arguments
    Replace unnecessary backticks and double quotes with single quotes
    Place && at beginning of lines (operator-linebreak)
    
    Add return of count at end of reduce function at line 68
    (fixes array-callback-return and consistent return errors)
    
    Rename const 'regex' to 'regexp' to fix no-shadow rule affecting
    lines 81,85

--- botEngine.js
+++ botEngine.js
@@ -1,92 +1,90 @@
-/* eslint-disable */
 const botCommands = [];
 
+let authorBuffer = [];
 
-let authorBuffer = []
-
-let creationsMessage = null
+let creationsMessage = null;
 
 const createAuthorEntry = function (message) {
   const entry = {
     author: message.author.id,
-    timeOut: false
-  }
+    timeOut: false,
+  };
 
-  setTimeout(function () {
-    entry.timeOut = true
-  }, 60000)
+  setTimeout(() => {
+    entry.timeOut = true;
+  }, 60000);
 
-  return entry
-}
+  return entry;
+};
 
 const flushAuthorEntries = function () {
-  authorBuffer = authorBuffer.filter(entry => entry.timeOut == false)
-}
+  authorBuffer = authorBuffer.filter((entry) => entry.timeOut === false);
+};
 
 function registerBotCommand(regex, fn) {
   botCommands.push({ regex, fn });
 }
 
 async function listenToMessages(client) {
-  client.on("message", async message => {
+  client.on('message', async (message) => {
     // Prevent bot from responding to its own messages
     if (message.author === client.user) {
       return;
     }
 
-    const regex = new RegExp("ok", "i");
-    const NOBOT_ROLE_ID = "783764176178774036";
+    const regexp = new RegExp('ok', 'i');
+    const NOBOT_ROLE_ID = '783764176178774036';
 
     // can't bot if user is NOBOT
     if (
-      message.author &&
-      message.author.lastMessage &&
-      message.author.lastMessage.member &&
-      message.author.lastMessage.member.roles &&
-      message.author.lastMessage.member.roles.has(NOBOT_ROLE_ID)
+      message.author
+      && message.author.lastMessage
+      && message.author.lastMessage.member
+      && message.author.lastMessage.member.roles
+      && message.author.lastMessage.member.roles.has(NOBOT_ROLE_ID)
     ) {
       return;
     }
 
     if (
-      message.channel.id === '693255421607280670' &&
-      message.member.roles.find(role => role.name === 'loki?')
+      message.channel.id === '693255421607280670'
+      && message.member.roles.find((role) => role.name === 'loki?')
     ) {
-      if (regex.test(message)) {
+      if (regexp.test(message)) {
         message.channel.send(`Hello there, ${message.author}! It seems you misunderstood our instructions. Please return to <#693244715839127653> and read carefully.`);
-      }
-      else {
+      } else {
         message.channel.send(`Hello ${message.author}! If you haven't yet, go read the <#693244715839127653> for instructions on how to access the rest of our discord server.
 If you are still having trouble after following the instructions, DM a Maintainer or Core member.`);
       }
       return;
-    } else if (message.channel.id === '627445384297316352') { // creations-showcase
+    } if (message.channel.id === '627445384297316352') { // creations-showcase
       if (creationsMessage) {
-        creationsMessage.delete()
+        creationsMessage.delete();
       }
-      creationsMessage = await message.channel.send("Reminder: This channel is for posting links to your creations only. You can discuss the projects posted here in the sibling channel <#634025871614803968>");
+      creationsMessage = await message.channel.send('Reminder: This channel is for posting links to your creations only. You can discuss the projects posted here in the sibling channel <#634025871614803968>');
       return;
     }
 
     const authorEntryCount = authorBuffer.reduce((count, current) => {
-      if (current.author == message.author.id) {
-        return count + 1
+      if (current.author === message.author.id) {
+        return count + 1;
       }
-    }, 0)
+      return count;
+    }, 0);
 
-    flushAuthorEntries()
+    flushAuthorEntries();
 
     if (authorEntryCount > 10) {
-      console.log('DENIED')
-      return
+      console.log('DENIED');
+      return;
     }
 
     botCommands.forEach(async ({ regex, fn }) => {
-      if (process.argv.includes("dev") && message.channel.type != 'dm') {
-        return
+      if (process.argv.includes('dev') && message.channel.type !== 'dm') {
+        return;
       }
       if (message.content.toLowerCase().match(regex)) {
-        authorBuffer.push(createAuthorEntry(message))
+        authorBuffer.push(createAuthorEntry(message));
         try {
           const response = await fn(message);
 
@@ -97,9 +95,8 @@ If you are still having trouble after following the instructions, DM a Maintaine
               console.log(e);
             }
           }
-        }
-        catch (e) {
-          console.log(e)
+        } catch (e) {
+          console.log(e);
         }
       }
     });
