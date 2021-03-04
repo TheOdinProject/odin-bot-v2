89270928ba6631bff328fec5da8304ee7b9880c0
Jonathan Colman <joncolman94@gmail.com>
2021-03-04 12:35:15 +0000

    Lint callbacks.js to pass ESLint
    Previous commit failed ESLint.
    Replaced template literals (`) with single quotes.
    Placed comma at end of command object.
    Placed semi-colon after command object declaration.

--- botCommands/callbacks.js
+++ botCommands/callbacks.js
@@ -1,10 +1,9 @@
-/* eslint-disable */
 const { registerBotCommand } = require('../botEngine.js');
 
 const command = {
   regex: /(?<!\S)\/callbacks(?!\S)/,
-  cb: () => `**DID SOMEONE SAY CALLBACKS?: https://briggs.dev/blog/understanding-callbacks**`
-}
+  cb: () => '**DID SOMEONE SAY CALLBACKS?: https://briggs.dev/blog/understanding-callbacks**',
+};
 
 registerBotCommand(command.regex, command.cb);
 
