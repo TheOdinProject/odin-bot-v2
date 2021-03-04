7237d43480b8b72e848bbe022f7daf7e80f2d9c1
Jonathan Colman <joncolman94@gmail.com>
2021-03-04 14:09:37 +0000

    Lint partyParrot.js and test to comply with ESlint
    Previously, partyParrot.js and test failed ESlint.
    
    Replace all unnecessary backticks and double quotes with single quotes.
    Remove unnecessary else after return in partyParrot.js
    Replace unary operator with '+= 1' assignment
    Place missing trailing commas.

--- botCommands/partyParrot.js
+++ botCommands/partyParrot.js
@@ -1,32 +1,30 @@
-/* eslint-disable */
-const { registerBotCommand } = require("../botEngine.js");
-const { randomInt } = require("./helpers.js");
+const { registerBotCommand } = require('../botEngine.js');
+const { randomInt } = require('./helpers.js');
 
 const command = {
   regex: /partyparrot|party_parrot|party parrot|oiseau/,
   cb: ({ content }) => {
     const parrots = [
-      "https://cultofthepartyparrot.com/parrots/hd/dadparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/parrot.gif",
-      "http://cultofthepartyparrot.com/parrots/shuffleparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/parrotcop.gif",
-      "http://cultofthepartyparrot.com/parrots/fiestaparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/explodyparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/aussieparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/slomoparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/stableparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/twinsparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/hd/dealwithitparrot.gif",
-      "http://cultofthepartyparrot.com/parrots/tripletsparrot.gif",
-      "http://emojis.slackmojis.com/emojis/images/1450738632/246/leftshark.png",
+      'https://cultofthepartyparrot.com/parrots/hd/dadparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/parrot.gif',
+      'http://cultofthepartyparrot.com/parrots/shuffleparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/parrotcop.gif',
+      'http://cultofthepartyparrot.com/parrots/fiestaparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/explodyparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/aussieparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/slomoparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/stableparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/twinsparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/hd/dealwithitparrot.gif',
+      'http://cultofthepartyparrot.com/parrots/tripletsparrot.gif',
+      'http://emojis.slackmojis.com/emojis/images/1450738632/246/leftshark.png',
     ];
 
-    if (content.match("!")) {
+    if (content.match('!')) {
       return `${parrots[0]}`;
-    } else {
-      const index = randomInt(parrots.length);
-      return `${parrots[index]}`;
     }
+    const index = randomInt(parrots.length);
+    return `${parrots[index]}`;
   },
 };
 
