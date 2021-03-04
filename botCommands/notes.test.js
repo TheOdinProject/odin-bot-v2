cea40faf4a38f002fd1889d5dc6c7519cd02ab9f
Jonathan Colman <joncolman94@gmail.com>
2021-03-04 13:57:41 +0000

    Lint notes.js and test to comply with ESlint
    notes.js and it's test previously failed ESlint
    
    Replaced uneccessary double quotes and backticks with single quotes.
    Removed block statement from notes.js, immediately returning string value.
    Placed missing semicolons and commas

--- botCommands/notes.test.js
+++ botCommands/notes.test.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-const command = require('./notes')
+const command = require('./notes');
 
 describe('/notes', () => {
   describe('regex', () => {
@@ -9,29 +8,29 @@ describe('/notes', () => {
       ['/notes @odin-bot'],
       ['@odin-bot /notes'],
     ])('correct strings trigger the callback', (string) => {
-      expect(command.regex.test(string)).toBeTruthy()
-    })
+      expect(command.regex.test(string)).toBeTruthy();
+    });
 
     it.each([
-      ["ntoes"],
-      ["notes"],
-      ["noets"],
-      ["/noets"],
-      ["/ntoes"],
-      ["/ notes"],
-      ["/anotes"]
+      ['ntoes'],
+      ['notes'],
+      ['noets'],
+      ['/noets'],
+      ['/ntoes'],
+      ['/ notes'],
+      ['/anotes'],
     ])("'%s' does not trigger the callback", (string) => {
-      expect(command.regex.test(string)).toBeFalsy()
-    })
+      expect(command.regex.test(string)).toBeFalsy();
+    });
 
     it.each([
       ['Check this out! /notes'],
       ['Don\'t worry about /notes'],
       ['Hey @odin-bot, /notes'],
-      ['/@odin-bot ^ /me /notes /tests$*']
+      ['/@odin-bot ^ /me /notes /tests$*'],
     ])("'%s' - command can be anywhere in the string", (string) => {
-      expect(command.regex.test(string)).toBeTruthy()
-    })
+      expect(command.regex.test(string)).toBeTruthy();
+    });
 
     it.each([
       ['@user/notes'],
@@ -39,15 +38,15 @@ describe('/notes', () => {
       ['/notesanillusion'],
       ['/notes/'],
       ['/notes*'],
-      ['/notes...']
+      ['/notes...'],
     ])("'%s' - command should be its own word/group - no leading or trailing characters", (string) => {
-      expect(command.regex.test(string)).toBeFalsy()
-    })
-  })
+      expect(command.regex.test(string)).toBeFalsy();
+    });
+  });
 
   describe('callback', () => {
     it('returns correct output', () => {
-      expect(command.cb()).toMatchSnapshot()
-    })
-  })
-})
+      expect(command.cb()).toMatchSnapshot();
+    });
+  });
+});
