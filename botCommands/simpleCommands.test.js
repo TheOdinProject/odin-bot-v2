97f1162dae7ce22953adf25d88a53f199c083af7
Brian Wisniewski <minidgenius@gmail.com>
2021-03-16 20:50:59 -0400

    Created "/xy" command and complimentary test

--- botCommands/simpleCommands.test.js
+++ botCommands/simpleCommands.test.js
@@ -831,3 +831,62 @@ describe('/justdoit', () => {
     });
   });
 });
+
+describe('/xy', () => {
+  describe('regex', () => {
+    it.each([
+      ['/xy'],
+      [' /xy'],
+      ['/xy @odin-bot'],
+      ['@odin-bot /xy'],
+    ])('correct strings trigger the callback', (string) => {
+      expect(commands.xy.regex.test(string)).toBeTruthy()
+    })
+    
+    it.each([
+      ['/yx'],
+      ['xy'],
+      ['/x'],
+      ['/xs'],
+      ['```function("/xy", () => {}```'],
+      ['/d'],
+      [''],
+      [' '],
+      [' /'],
+      ['@odin-bot / xy'],
+      ['/x&y'],
+      ['/x^y'],
+      ['/xy!'],
+      ['@odin-bot/ xy'],
+      ['https://xy.com'],
+    ])("'%s' does not trigger the callback", (string) => {
+      expect(commands.xy.regex.test(string)).toBeFalsy()
+    })
+
+    it.each([
+      ['Check this out! /xy'],
+      ['Don\'t worry about /xy'],
+      ['Hey @odin-bot, /xy'],
+      ['/@odin-bot ^ /me /xy /tests$*']
+    ])("'%s' - command can be anywhere in the string", (string) => {
+      expect(commands.xy.regex.test(string)).toBeTruthy()
+    })
+
+    it.each([
+      ['@user/xy'],
+      ['it\'s about/xy'],
+      ['/xyisanillusion'],
+      ['/xy/'],
+      ['/xy*'],
+      ['/xy...']
+    ])("'%s' - command should be its own word/group - no leading or trailing characters", (string) => {
+      expect(commands.xy.regex.test(string)).toBeFalsy()
+    })
+  })
+
+  describe('callback', () => {
+    it('returns correct output', () => {
+      expect(commands.xy.cb()).toMatchSnapshot()
+    })
+  })
+})
