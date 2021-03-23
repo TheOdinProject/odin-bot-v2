20c5525329710a4c0682d675a6dd41da47f7d522
Kevin <kbrowne97@gmail.com>
2021-03-23 13:14:57 -0400

    removes redundant test case

--- botCommands/faq.test.js
+++ botCommands/faq.test.js
@@ -8,7 +8,6 @@ describe('/faq', () => {
     ['/faq @odin-bot'],
     [' /faq @odin-bot'],
     ['/faq @user'],
-    ['/faq @user @user'],
   ])("'%s' triggers the callback", (string) => {
     expect(command.regex.test(string)).toBeTruthy();
   });
