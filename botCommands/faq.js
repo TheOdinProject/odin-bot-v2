3b39c53158844dd0b886a326431758b31a58c2ea
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-04-03 23:32:00 +0530

    Fix /faq to use URL

--- botCommands/faq.js
+++ botCommands/faq.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js');
-const { registerBotCommand } = require('../botEngine.js');
+const Discord = require("discord.js");
+const { registerBotCommand } = require("../botEngine.js");
 
 const command = {
   regex: /(?<!\S)\/faq(?!\S)/,
   cb: async ({ mentions }) => {
-    let users = '';
+    let users = "";
     const mentionedUsers = mentions.users.array();
     if (mentionedUsers.length >= 3) {
       mentionedUsers.forEach((user, index) => {
@@ -31,8 +31,8 @@ const command = {
       .setTitle('Frequently Asked Questions')
       .setDescription(
         !users
-          ? '**The definition of insanity is answering the same question over and over again when we have an <#823266307293839401>!  Help us stay sane by giving it a read.**'
-          : `**The definition of insanity is answering the same question over and over again when we have an <#823266307293839401>!${users}, help us stay sane by giving it a read.**`,
+          ? '**The definition of insanity is answering the same question over and over again when we have an [#faq](https://discord.com/channels/505093832157691914/823266307293839401/823266549912829992)!  Help us stay sane by giving it a read.**'
+          : `**The definition of insanity is answering the same question over and over again when we have an [#faq](https://discord.com/channels/505093832157691914/823266307293839401/823266549912829992)!${users}, help us stay sane by giving it a read.**`
       );
 
     return faqCommandEmbed;
