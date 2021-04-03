671879159fcc2052cbb1a871ab27751f108d2de5
Michał Długosz <mdugosz@gmail.com>
2021-04-03 23:34:39 +0200

    Change embed color and add link to description

--- botCommands/windows.js
+++ botCommands/windows.js
@@ -5,9 +5,9 @@ const command = {
   regex: /(?<!\S)\/windows(?!\S)/,
   cb: () => {
     const windowsEmbed = new Discord.MessageEmbed()
-      .setColor('#ff471a')
+      .setColor('#cc9543')
       .setTitle('Windows')
-      .setDescription('**The Odin Project does not support Windows, WSL, or any OS outside of our recommendations**. We are happy to assist with any questions about installing a VM or dual booting Linux.')
+      .setDescription('**The Odin Project does not support Windows, WSL, or any OS outside of our recommendations**. We are happy to assist with any questions about installing a VM or dual booting Linux. <https://www.theodinproject.com/courses/foundations/lessons/installation-overview#os-options>')
       .setURL('https://www.theodinproject.com/courses/foundations/lessons/installation-overview#os-options');
 
     return windowsEmbed;
