b6431cc2e9944765869989f8d3c7c654e1916784
mercerary <mercerary@tutanota.com>
2022-03-20 22:51:44 +0200

    Fix text structure and spacing

--- botCommands/simpleCommands.js
+++ botCommands/simpleCommands.js
@@ -52,16 +52,17 @@ const data = {
       .setDescription(`
 Instead of asking if anyone can help you, ask your question outright so people can help you!
       
-**Bad**: "Hey, does anyone know how to set CSS styles with Javascript?"
+**Bad**:
+"Hey, does anyone know how to set CSS styles with Javascript?"
     
 **Good**: 
-"Hey, I'm having trouble setting CSS styles via Javascript.
+"Hey, I'm having trouble setting CSS styles via Javascript."
   
 Here's my code: 
 \`\`\`Code snippet\`\`\`\
   
 And here is the error I'm receiving:
-\`Cannot set attribute 'style' of null\` "
+\`\`\`Cannot set attribute 'style' of null\`\`\`
   
 **https://www.dontasktoask.com/**
         `);
