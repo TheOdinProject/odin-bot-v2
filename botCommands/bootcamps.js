4aabfe244a6625f4872d2f3e6fad214b4d49aae5
Asartea <76259120+Asartea@users.noreply.github.com>
2023-07-04 21:21:11 +0200

    Bootcamps: change link to Web Archive

--- botCommands/bootcamps.js
+++ botCommands/bootcamps.js
@@ -2,7 +2,7 @@ const { registerBotCommand } = require('../botEngine');
 
 const command = {
   regex: /(?<!\S)!bootcamps?(?!\S)/,
-  cb: () => 'Friends don\'t let friends commit to bootcamps without being informed.  https://twitter.com/lzsthw/status/1212284566431576069',
+  cb: () => 'Friends don\'t let friends commit to bootcamps without being informed.  https://web.archive.org/web/20220410160426/https://twitter.com/lzsthw/status/1212284566431576069',
 };
 
 registerBotCommand(command.regex, command.cb);
