5319d5f4916b6644cadea98b2563363271dcb6f8
Asartea <76259120+Asartea@users.noreply.github.com>
2023-07-22 18:57:00 +0200

    Move WSL to the supported category

--- new-era-commands/slash/windows.js
+++ new-era-commands/slash/windows.js
@@ -8,7 +8,7 @@ module.exports = {
     const windowsEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('Windows')
-      .setDescription('**The Odin Project does not support Windows, WSL, or any OS outside of our recommendations**. We are happy to assist with any questions about installing a VM or dual booting Linux. <https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options>')
+      .setDescription('**The Odin Project does not support Windows, or any OS outside of our recommendations**. We are happy to assist with any questions about installing a VM, using WSL, or dual booting Linux. <https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options>')
       .setURL('https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options');
 
     await interaction.reply({ embeds: [windowsEmbed] });
