1a225607b0e601803aa69043b30b2ba14f1098ac
Manon <manonlefkos@gmail.com>
2023-08-03 00:02:28 +0200

    update contribute command

--- new-era-commands/slash/contribute.js
+++ new-era-commands/slash/contribute.js
@@ -5,56 +5,36 @@ module.exports = {
     .setName('contribute')
     .setDescription('Information on how to contribute to TOP on GitHub'),
   execute: async (interaction) => {
-    const debugEmbed = new EmbedBuilder()
+    const contributingEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('Contributing to The Odin Project Repositories')
-      .setURL('https://www.theodinproject.com/contributing')
+      .setURL('https://github.com/TheOdinProject')
       .setDescription(`
+      
+      ** How to contribute **
 
-        You too can contribute to our repositories on GitHub! Whether you've spotted a way to enhance our content or encountered a bug, your contributions are priceless! 
-        
-        If you are familiar with forking, cloning, and opening a pull request, you should be ready to make your (first) open-source contributions.
+      To contribute to The Odin Project, check out our repositories on GitHub:
+      
+      • [TOP curriculum repository](https://github.com/TheOdinProject/curriculum)
+      • [TOP main website repository](https://github.com/TheOdinProject/theodinproject)
+      • [TOP odin-bot Repository](https://github.com/TheOdinProject/odin-bot-v2)
+      
+      Make sure to read the [contributing guide](https://github.com/TheOdinProject/.github/blob/main/CONTRIBUTING.md) before getting started.
 
-        **TOP Contributing Guide**
-        To learn more about how to contribute, [you can find our contributing guide here](https://github.com/TheOdinProject/.github/blob/main/CONTRIBUTING.md).
+      To find issues ready to be worked on, go to the \`issues\` tab in each repository. You can also create new issues or propose suggestions there. 
+      
+      ** First time contributors **
 
-        **Open Issues**
-        On each of the repositories shared below, you can find a tab called \`issues\` where you will see all open issues for the repository. 
+      If you're a first-time contributor, you can look for issues labeled \`good first issue\`. 
+      These are beginner-friendly and perfect for getting started with open source contributions.
+      
+      ** Top-contributors role **
+      
+      You will recognize our TOP-Contributors by their special role which you can learn more about in our [#roles](https://discord.com/channels/505093832157691914/936424264180060200) channel.
 
-        **First Contributors**
-        Open issues marked as \`good first issue\` will often be a great first issue to pick up. Follow this link [to see the curriculum's good first issues](https://github.com/TheOdinProject/curriculum/issues?q=is%3Aissue+is%3Aopen+label%3A%22Type%3A+Good+First+Issue%22).
-        You can comment on an issue if you wish to be assigned.
-        \u200b
-      `)
-      .addFields(
-        [{
-          name: 'TOP Curriculum',
-          value: '[The TOP curriculum repo](https://github.com/TheOdinProject/curriculum) contains the lessons and projects on the website. Here you can add lesson content, resources and work on open issues. You can also make suggestions or open an issue of your own.',
-          inline: true,
-        },
-        {
-          name: 'TOP Main Website',
-          value: '[The TOP main website repo](https://github.com/TheOdinProject/theodinproject) is a Ruby on Rails application. Here you can contribute towards website features, bugs or open your own issue for features, suggestions or bugs you\'ve encountered.',
-          inline: true,
-        },
-        {
-          name: 'Odin Bot',
-          value: '[The Odin bot](https://github.com/TheOdinProject/odin-bot-v2) is also open source, as a cherished member of our server our bot surely appreciates the love and care you put into making it even better.',
-          inline: true,
-        },
-        {
-          name: 'Contribution Opportunities',
-          value: 'Keep an eye on our [#contribution-opportunities channel](https://discord.com/channels/505093832157691914/700355756188368926) to see if we have posted any open issues that could use some help.',
-          inline: true,
-        },
-        {
-          name: 'TOP Contributors',
-          value: 'You may encounter someone with the role of top-contributor in this server. This is a role to recognize users who have made numerous contributions to our public repositories. To learn more about this role, head over to [our #roles channel](936424264180060200)',
-          inline: true,
-        },
-        ],
-      );
+      We hope to see you on GitHub!
+      `);
 
-    await interaction.reply({ embeds: [debugEmbed] });
+    await interaction.reply({ embeds: [contributingEmbed] });
   },
 };
