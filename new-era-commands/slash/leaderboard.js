f42d331bcd29c2cab41bbf18eaf7ed0acc6d4e17
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-03 02:14:47 +0200

    Replaced old leaderboard command with new slash-command

--- new-era-commands/slash/leaderboard.js
+++ new-era-commands/slash/leaderboard.js
@@ -4,12 +4,6 @@ const config = require('../../config');
 
 axios.default.defaults.headers.get.Authorization = `Token ${config.pointsbot.token}`;
 
-function getLeaderboardInformation(interaction) {
-  if (interaction.options.getSubcommand() === 'user') {
-    displayUserRank(interaction);
-  }
-}
-
 async function displayUserRank(interaction) {
   const user = interaction.options.getUser('name');
 
@@ -17,10 +11,66 @@ async function displayUserRank(interaction) {
     const response = await axios.get(`https://www.theodinproject.com/api/points/${user.id}`);
     const userPoints = response.data.points !== undefined ? response.data.points : 0;
     const rank = response.data.rank !== undefined ? `${response.data.rank} - ` : '';
-    interaction.reply(`${rank}${user.username} has ${userPoints} point${userPoints === 1 ? '' : 's'}!`);
+
+    const userRankReply = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Leaderboard')
+      .addFields([{ name: 'User Rank', value: `${rank}${user.username} has ${userPoints} point${userPoints === 1 ? '' : 's'}!` }]);
+
+    await interaction.reply({ embeds: [userRankReply] });
   } catch (err) {
     console.log(err.stack);
     console.log('@ new-era-commands/slash/leaderboard.js function displayUserRank()');
+  }
+}
+
+async function displayServerRanking(interaction) {
+  const limitOption = interaction.options.getInteger('limit');
+  const limit = limitOption <= 25 && limitOption > 0 ? limitOption : 25;
+
+  const offsetOption = interaction.options.getInteger('offset');
+  const offset = offsetOption != null ? offsetOption : 0;
+
+  try {
+    const users = await axios.get('https://www.theodinproject.com/api/points');
+    const data = users.data.filter((user) => interaction.guild.members.cache.get(user.discord_id));
+    let usersList = '';
+
+    for (let i = offset; i < limit + offset; i += 1) {
+      const user = data[i];
+      if (user) {
+        const member = interaction.guild.members.cache.get(user.discord_id);
+        const username = member ? member.displayName.replace(/!/g, '!') : '';
+
+        if (username.length !== 0) {
+          if (i === 0) {
+            usersList += `${i + 1} - ${username} [${user.points} points] :tada: \n`;
+          } else {
+            usersList += `${i + 1} - ${username} [${user.points} points] \n`;
+          }
+        } else {
+          usersList += 'User has left the server. \n';
+        }
+      }
+    }
+
+    const leaderboardReply = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Leaderboard')
+      .addFields([{ name: 'Server Ranking', value: usersList || 'Be the first to earn a point!' }]);
+
+    await interaction.reply({ embeds: [leaderboardReply] });
+  } catch (err) {
+    console.log(err.stack);
+    console.log('@ new-era-commands/slash/leaderboard.js function displayLeaderboardRanking()');
+  }
+}
+
+function getLeaderboardInformation(interaction) {
+  if (interaction.options.getSubcommand() === 'user') {
+    displayUserRank(interaction);
+  } else {
+    displayServerRanking(interaction);
   }
 }
 
@@ -37,10 +87,30 @@ function userRanking(command) {
   return command;
 }
 
+function serverRanking(command) {
+  command.setName('ranking')
+    .setDescription('Odin Leaderboard Ranking')
+    .addIntegerOption((option) => {
+      option.setName('limit')
+        .setDescription('Limit the result. Max is 25')
+        .setRequired(false);
+      return option;
+    })
+    .addIntegerOption((option) => {
+      option.setName('offset')
+        .setDescription('Offset is the starting position in the ranking')
+        .setRequired(false);
+      return option;
+    });
+
+  return command;
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('leaderboard')
     .setDescription('Placehodler')
-    .addSubcommand(userRanking),
+    .addSubcommand(userRanking)
+    .addSubcommand(serverRanking),
   execute: getLeaderboardInformation,
 };
