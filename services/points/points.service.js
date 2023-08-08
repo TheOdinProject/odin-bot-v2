09143e4b2781d29307643f6787cc657dba0345d4
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-08 18:18:30 +0200

    Redirect error handling from service to slashcommand

--- services/points/points.service.js
+++ services/points/points.service.js
@@ -44,30 +44,25 @@ Our goal is to maintain a positive and supportive community, where help and cont
   }
 
   static async displayUserPoints(interaction) {
-    try {
-      const user = interaction.options.getUser('name');
-      const response = await axios.get(`https://www.theodinproject.com/api/points/${user.id}`);
-      const { rank } = response.data;
+    const user = interaction.options.getUser('name');
+    const response = await axios.get(`https://www.theodinproject.com/api/points/${user.id}`);
+    const { rank } = response.data;
 
-      let { points } = response.data;
-      points = points !== undefined ? points : 0;
+    let { points } = response.data;
+    points = points !== undefined ? points : 0;
 
-      const userRankReply = new EmbedBuilder()
-        .setColor('#cc9543')
-        .setTitle(`${user.username} Leaderboard!`)
-        .addFields([{ name: 'Points', value: `${user.username} has ${points === undefined ? 0 : points} point${points === 1 ? '' : 's'}!` }]);
+    const userRankReply = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle(`${user.username} Leaderboard!`)
+      .addFields([{ name: 'Points', value: `${user.username} has ${points === undefined ? 0 : points} point${points === 1 ? '' : 's'}!` }]);
 
-      if (rank === undefined) {
-        userRankReply.addFields([{ name: 'Rank', value: `${user.username} currently has no rank in the leaderboard!` }]);
-      } else {
-        userRankReply.addFields([{ name: 'Rank', value: `${user.username} is ranked number ${rank}!` }]);
-      }
-
-      await interaction.reply({ embeds: [userRankReply] });
-    } catch (err) {
-      console.log(err.stack);
-      console.log('@ new-era-commands/slash/leaderboard.js function displayUserRank()');
+    if (rank === undefined) {
+      userRankReply.addFields([{ name: 'Rank', value: `${user.username} currently has no rank in the leaderboard!` }]);
+    } else {
+      userRankReply.addFields([{ name: 'Rank', value: `${user.username} is ranked number ${rank}!` }]);
     }
+
+    await interaction.reply({ embeds: [userRankReply] });
   }
 
   static getUsersList(users, limit, offset, interaction) {
@@ -88,32 +83,27 @@ Our goal is to maintain a positive and supportive community, where help and cont
   }
 
   static async displayLeaderboard(interaction) {
-    try {
-      const response = await axios.get('https://www.theodinproject.com/api/points');
-      // eslint-disable-next-line max-len
-      const users = response.data.filter((user) => interaction.guild.members.cache.get(user.discord_id));
+    const response = await axios.get('https://www.theodinproject.com/api/points');
+    // eslint-disable-next-line max-len
+    const users = response.data.filter((user) => interaction.guild.members.cache.get(user.discord_id));
 
-      let limit = interaction.options.getInteger('limit');
-      limit = limit <= 25 && limit > 0 ? limit : 25;
-      limit = limit <= users.length ? limit : users.length;
+    let limit = interaction.options.getInteger('limit');
+    limit = limit <= 25 && limit > 0 ? limit : 25;
+    limit = limit <= users.length ? limit : users.length;
 
-      let offset = interaction.options.getInteger('offset');
-      offset = offset > 0 ? offset : 0;
-      // Always show the last members if offset too high
-      offset = offset + limit < users.length ? offset : Math.max(0, users.length - limit);
+    let offset = interaction.options.getInteger('offset');
+    offset = offset > 0 ? offset : 0;
+    // Always show the last members if offset too high
+    offset = offset + limit < users.length ? offset : Math.max(0, users.length - limit);
 
-      const usersList = PointsService.getUsersList(users, limit, offset, interaction);
+    const usersList = PointsService.getUsersList(users, limit, offset, interaction);
 
-      const leaderboardReply = new EmbedBuilder()
-        .setColor('#cc9543')
-        .setTitle('Leaderboard')
-        .addFields([{ name: 'Server Ranking', value: usersList || 'Be the first to earn a point!' }]);
+    const leaderboardReply = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Leaderboard')
+      .addFields([{ name: 'Server Ranking', value: usersList || 'Be the first to earn a point!' }]);
 
-      await interaction.reply({ embeds: [leaderboardReply] });
-    } catch (err) {
-      console.log(err.stack);
-      console.log('@ new-era-commands/slash/leaderboard.js function displayLeaderboardRanking()');
-    }
+    await interaction.reply({ embeds: [leaderboardReply] });
   }
 }
 
