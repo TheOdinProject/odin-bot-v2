52d51c2f8eb5fb6adfcaca670719b20ce9bceff1
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-08 17:34:47 +0200

    Refactor naming

--- new-era-commands/slash/points.js
+++ new-era-commands/slash/points.js
@@ -1,4 +1,4 @@
-const SlashCommandBuilder = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const PointsService = require('../../services/points');
 
 function userPoints(command) {
