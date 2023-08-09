0de7379dea110fbe6ebf3f828ff95f111d20919f
Manon <manonlefkos@gmail.com>
2023-08-09 23:58:00 +0200

    add google slash command with mention

---
+++ new-era-commands/slash/google.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('google')
+    .setDescription('Search Google')
+    .addStringOption((option) => option
+      .setName('prompt')
+      .setDescription('term to search')
+      .setRequired(true))
+    .addUserOption((option) => option.setName('user').setDescription('user to ping')),
+  execute: async (interaction) => {
+    const userId = interaction.options.getUser('user')?.id;
+    const prompt = interaction.options.getString('prompt');
+    const searchUrl = `https://www.google.com/search?q=${prompt.replaceAll(' ', '+')}`;
+
+    const searchEmbed = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Search Google')
+      .setDescription(`This [Google query for "${prompt}"](${searchUrl}) might help you find what you're looking for.`);
+    await interaction.reply({
+      content: userId ? `<@${userId}>` : '',
+      embeds: [searchEmbed],
+    });
+  },
+};
