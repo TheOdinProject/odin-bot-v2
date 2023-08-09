3324dd6a537f446214f2dc014b5e31ba0f073e9d
Manon <manonlefkos@gmail.com>
2023-08-09 22:20:35 +0200

    update search command to contain embed and descriptive link

--- new-era-commands/slash/searchtop.js
+++ new-era-commands/slash/searchtop.js
@@ -1,18 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('searchtop')
     .setDescription('Search for text in The Odin Project website (with Google)')
-    .addStringOption((option) => option.setName('prompt').setDescription('term to search').setRequired(true))
+    .addStringOption((option) => option
+      .setName('prompt')
+      .setDescription('term to search')
+      .setRequired(true))
     .addUserOption((option) => option.setName('user').setDescription('user to ping')),
   execute: async (interaction) => {
     const userId = interaction.options.getUser('user')?.id;
     const prompt = interaction.options.getString('prompt');
     const searchUrl = `https://www.google.com/search?q=site:theodinproject.com+${prompt.replaceAll(' ', '+')}`;
 
-    await interaction.reply(
-      userId ? `<@${userId}>\nSearching The Odin Project for \`${prompt}\`: <${searchUrl}>` : `Searching The Odin Project for \`${prompt}\`: <${searchUrl}>`,
-    );
+    const searchEmbed = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Search TOP')
+      .setDescription(
+        userId
+          ? `<@${userId}>\nHere are the [Google results for TOP lessons containing "${prompt}"](${searchUrl})`
+          : `Here are the [Google results for TOP lessons containing "${prompt}"](${searchUrl})`,
+      );
+    await interaction.reply({ embeds: [searchEmbed] });
   },
 };
