6ec10e6347c84afc69206445adf85fa2bd25b457
Manon <81025586+ManonLef@users.noreply.github.com>
2023-08-20 23:12:02 +0200

    Update new-era-commands/slash/notes.js

--- new-era-commands/slash/notes.js
+++ new-era-commands/slash/notes.js
@@ -9,7 +9,7 @@ module.exports = {
       .setColor('#cc9543')
       .setTitle('To note, or not to note, that is the question')
       .setDescription(`
-Please read this [discord message about what we think you should consider about taking notes](https://discord.com/channels/505093832157691914/505093832157691916/768161823366578176)
+Please read this [Discord message about what we think you should consider about taking notes](https://discord.com/channels/505093832157691914/505093832157691916/768161823366578176)
       `);
 
     await interaction.reply({ embeds: [freelanceEmbed] });
