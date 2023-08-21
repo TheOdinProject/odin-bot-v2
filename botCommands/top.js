4c924ace36f638c43adfa2a22690026801e000f2
Manon <manonlefkos@gmail.com>
2023-08-21 22:28:08 +0200

    edit top command to embed and update snapshot

--- botCommands/top.js
+++ botCommands/top.js
@@ -1,8 +1,17 @@
+const Discord = require('discord.js');
 const { registerBotCommand } = require('../botEngine');
 
 const command = {
   regex: /(?<!\S)!top(?!\S)/,
-  cb: () => 'The Odin Project. Your Career in Web Development Starts Here. <https://www.theodinproject.com> ',
+  cb: () => {
+    const topLegacyEmbed = new Discord.EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('The Odin Project').setDescription(`
+For more information about The Odin Project, visit our site:
+[Your Career in Web Development Starts Here](https://www.theodinproject.com/)    
+    `);
+    return { embeds: [topLegacyEmbed] };
+  },
 };
 
 registerBotCommand(command.regex, command.cb);
