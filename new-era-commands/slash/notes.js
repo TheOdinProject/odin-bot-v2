95636bdbb5bc879c097fe0c9d0785aefcca2dae1
Manon <manonlefkos@gmail.com>
2023-09-26 20:35:06 +0200

    update notes embed name and simplify userID

--- new-era-commands/slash/notes.js
+++ new-era-commands/slash/notes.js
@@ -6,9 +6,9 @@ module.exports = {
     .setDescription('Things to consider about taking notes')
     .addUserOption((option) => option.setName('user').setDescription('user to ping')),
   execute: async (interaction) => {
-    const userId = interaction.options.getUser('user')?.id;
+    const userId = interaction.options.getUser('user');
 
-    const freelanceEmbed = new EmbedBuilder()
+    const notesEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('To note, or not to note, that is the question')
       .setDescription(`
@@ -16,8 +16,8 @@ Please read this [Discord message about what we think you should consider about 
       `);
 
     await interaction.reply({
-      content: userId ? `<@${userId}>` : '',
-      embeds: [freelanceEmbed],
+      content: userId ? `${userId}` : '',
+      embeds: [notesEmbed],
     });
   },
 };
