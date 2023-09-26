84e4d7e1989e81e5ed89ac19df5184e6f7892998
Manon <manonlefkos@gmail.com>
2023-09-26 20:32:12 +0200

    freelance cmd: remove unneeded code from getUser and response

--- new-era-commands/slash/freelance.js
+++ new-era-commands/slash/freelance.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription('Things to consider about freelancing')
     .addUserOption((option) => option.setName('user').setDescription('user to ping')),
   execute: async (interaction) => {
-    const userId = interaction.options.getUser('user')?.id;
+    const userId = interaction.options.getUser('user');
 
     const freelanceEmbed = new EmbedBuilder()
       .setColor('#cc9543')
@@ -16,7 +16,7 @@ Here are some things that should be considered before getting into freelancing: 
       `);
 
     await interaction.reply({
-      content: userId ? `<@${userId}>` : '',
+      content: userId ? `${userId}` : '',
       embeds: [freelanceEmbed],
     });
   },
