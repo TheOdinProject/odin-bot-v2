3bffdf52ac57dc9caea89a7e9ce1bc00e8ec9ee0
Manon <manonlefkos@gmail.com>
2023-09-28 15:00:51 +0200

    update !google to output same format as slash /google command

--- botCommands/google.js
+++ botCommands/google.js
@@ -1,16 +1,17 @@
+const { EmbedBuilder } = require('discord.js');
 const { registerBotCommand } = require('../botEngine');
 
 const command = {
   regex: /\B!google\s+.+/,
   cb: ({ content }) => {
-    const transform = (transformContent) => {
-      const query = transformContent.split(' ').map(encodeURIComponent).join('+');
-      return `**This query might help you find what you're looking for >** <https://google.com/search?q=${query}>`;
-    };
-
-    const query = content.match(/\B!google\s+(.+)/)[1];
-    return `${transform(query)}`;
-  },
+    const prompt = content.match(/\B!google\s+(.+)/)[1];
+    const searchUrl = `https://www.google.com/search?q=${prompt.replaceAll(' ', '+')}`;
+    const googleEmbed = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Search Google')
+      .setDescription(`This [Google query for "${prompt}"](${searchUrl}) might help you find what you're looking for.`);
+    return { embeds: [googleEmbed]};
+  }
 };
 registerBotCommand(command.regex, command.cb);
 
