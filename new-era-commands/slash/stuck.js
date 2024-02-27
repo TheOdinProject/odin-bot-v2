55c527228db0459a188a877438da2c81073d4ffb
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-27 19:30:04 +0000

    Replace attachment builder with Statically link

--- new-era-commands/slash/stuck.js
+++ new-era-commands/slash/stuck.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
-const { join } = require('path');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,8 +8,6 @@ module.exports = {
   execute: async (interaction) => {
     const userId = interaction.options.getUser('user');
 
-    const stuckImage = new AttachmentBuilder(join(__dirname, 'images/stuck.jpg'));
-
     const stuckEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('I\'m stuck - what can I do?')
@@ -19,12 +16,13 @@ Have a look through the following flowchart (click to enlarge) for guidance in h
 
 If you would like to ask for help, there is a very good question template you can find via the \`/question\` command which can help you include as much relevant context and structure your question in a way that makes it much easier for others to assist you.
       `)
-      .setImage('attachment://stuck.jpg');
+      .setImage(
+        'https://cdn.statically.io/gh/TheOdinProject/odin-bot-v2/03c97a193c751d1cdffa81c3d3bf84e00291cd26/new-era-commands/slash/images/stuck.jpg'
+      );
 
     await interaction.reply({
       content: userId ? `${userId}` : '',
       embeds: [stuckEmbed],
-      files: [stuckImage],
     });
   },
 };
