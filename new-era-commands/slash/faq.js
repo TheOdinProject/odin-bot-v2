005cf0eeff106cd99869cec695bd3a5cfb1af4aa
Fred Love <justwavethings@proton.me>
2024-03-08 10:00:22 -0500

    /faq - remove quote, update title, update link to channel id (#505)
    * update title and description
    
    * fix formatting/remove unnecessary line breaks
    
    * replace double quotes with single quotes
    
    * more formatting fixes

--- new-era-commands/slash/faq.js
+++ new-era-commands/slash/faq.js
@@ -10,13 +10,9 @@ module.exports = {
 
     const faqEmbed = new EmbedBuilder()
       .setColor('#cc9543')
-      .setTitle('Our TOP Discord FAQ')
-      .setDescription(`
-*"The definition of insanity is answering the same question over and over again"*
+      .setTitle('TOP FAQ')
+      .setDescription('We have a channel with frequently asked questions - <#823266307293839401>!');
 
-We have a [channel with frequently asked questions](https://discord.com/channels/505093832157691914/823266307293839401/823266549912829992)!
-Help us stay sane by giving it a read.
-      `);
     await interaction.reply({
       content: userId ? `${userId}` : '',
       embeds: [faqEmbed],
