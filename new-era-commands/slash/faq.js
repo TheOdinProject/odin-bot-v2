e33ed338a0639c34873989698cb90e7539549ff0
JustWaveThings <justwavethings@proton.me>
2024-03-08 09:41:16 -0500

    replace double quotes with single quotes

--- new-era-commands/slash/faq.js
+++ new-era-commands/slash/faq.js
@@ -1,22 +1,24 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("faq")
-    .setDescription("Provides a link to our FAQ channel")
-    .addUserOption((option) =>
-      option.setName("user").setDescription("user to ping")
+    .setName('faq')
+    .setDescription('Provides a link to our FAQ channel')
+    .addUserOption(option =>
+      option.setName('user').setDescription('user to ping')
     ),
-  execute: async (interaction) => {
-    const userId = interaction.options.getUser("user");
+  execute: async interaction => {
+    const userId = interaction.options.getUser('user');
 
     const faqEmbed = new EmbedBuilder()
-      .setColor("#cc9543")
-      .setTitle("TOP FAQ")
-      .setDescription("We have a channel with frequently asked questions - <#823266307293839401>!")
+      .setColor('#cc9543')
+      .setTitle('TOP FAQ')
+      .setDescription(
+        'We have a channel with frequently asked questions - <#823266307293839401>!'
+      );
 
     await interaction.reply({
-      content: userId ? `${userId}` : "",
+      content: userId ? `${userId}` : '',
       embeds: [faqEmbed],
     });
   },
