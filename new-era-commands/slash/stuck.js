21a9cf22e8c1d624a8d9400c8875a5b4237e6dd4
Cake <cakemancerr@gmail.com>
2024-03-08 16:50:42 +0100

    update stuck command img

--- new-era-commands/slash/stuck.js
+++ new-era-commands/slash/stuck.js
@@ -17,7 +17,7 @@ Have a look through the following flowchart (click to enlarge) for guidance in h
 If you would like to ask for help, there is a very good question template you can find via the \`/question\` command which can help you include as much relevant context and structure your question in a way that makes it much easier for others to assist you.
       `)
       .setImage(
-        'https://cdn.statically.io/gh/TheOdinProject/odin-bot-v2/03c97a193c751d1cdffa81c3d3bf84e00291cd26/new-era-commands/slash/images/stuck.jpg'
+        'https://cdn.statically.io/gh/TheOdinProject/curriculum/978ba0e91a6a47452de819246f4c495e6b092f54/foundations/introduction/motivation_and_mindset/imgs/00.png'
       );
 
     await interaction.reply({
