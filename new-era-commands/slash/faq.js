bd796866770359e8775c4f5a95b0c073e37f5e26
JustWaveThings <justwavethings@proton.me>
2024-03-08 09:52:20 -0500

    more formatting fixes

--- new-era-commands/slash/faq.js
+++ new-era-commands/slash/faq.js
@@ -4,18 +4,14 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('faq')
     .setDescription('Provides a link to our FAQ channel')
-    .addUserOption(option =>
-      option.setName('user').setDescription('user to ping')
-    ),
-  execute: async interaction => {
+    .addUserOption((option) => option.setName('user').setDescription('user to ping')),
+  execute: async (interaction) => {
     const userId = interaction.options.getUser('user');
 
     const faqEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('TOP FAQ')
-      .setDescription(
-        'We have a channel with frequently asked questions - <#823266307293839401>!'
-      );
+      .setDescription('We have a channel with frequently asked questions - <#823266307293839401>!');
 
     await interaction.reply({
       content: userId ? `${userId}` : '',
