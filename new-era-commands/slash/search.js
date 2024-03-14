04613cd147c0bba9a89e42c2a4f2fb1b029a285b
Cake <cakemancerr@gmail.com>
2024-03-15 00:02:03 +0100

    refactor the site options to subcommands

--- new-era-commands/slash/search.js
+++ new-era-commands/slash/search.js
@@ -1,70 +1,81 @@
-const { ButtonStyle } = require("discord.js");
 const { ActionRowBuilder } = require("discord.js");
 const { ButtonBuilder } = require("discord.js");
-const { SlashCommandBuilder } = require("discord.js");
+const { ButtonStyle, SlashCommandBuilder } = require("discord.js");
 
-const searchSites = [
-  { name: "Google", value: "https://www.google.com/search?q=" },
-  {
+const searchSites = {
+  google: {
+    name: "Google",
+    url: "https://www.google.com/search?q=",
+  },
+  top: {
     name: "TOP",
-    value: "https://www.google.com/search?q=site:theodinproject.com+",
+    url: "https://www.google.com/search?q=site:theodinproject.com+",
   },
-  {
+  mdn: {
     name: "MDN",
-    value: "https://developer.mozilla.org/en-US/search?q=",
+    url: "https://developer.mozilla.org/en-US/search?q=",
   },
-];
-
-function createSearchSite({ url, prompt }) {
-  const { name } = searchSites.find((s) => s.value === url);
-
-  return {
-    get description() {
-      return `${name} results for "${prompt}"`;
-    },
-    get searchURL() {
-      return `${url}${prompt.replaceAll(" ", "+")}`;
-    },
-  };
-}
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("search")
     .setDescription("Search in a specific site")
-    .addStringOption((option) =>
-      option
-        .setName("site")
-        .setDescription("site to search on")
-        .setChoices(...searchSites)
-        .setRequired(true),
+    .addSubcommand((command) =>
+      command
+        .setName("google")
+        .setDescription("search Google")
+        .addStringOption((option) =>
+          option
+            .setName("prompt")
+            .setDescription("term to search")
+            .setRequired(true),
+        )
+        .addUserOption((option) =>
+          option.setName("user").setDescription("user to ping"),
+        ),
     )
-    .addStringOption((option) =>
-      option
-        .setName("prompt")
-        .setDescription("term to search")
-        .setRequired(true),
+    .addSubcommand((command) =>
+      command
+        .setName("top")
+        .setDescription("search TOP")
+        .addStringOption((option) =>
+          option
+            .setName("prompt")
+            .setDescription("term to search")
+            .setRequired(true),
+        )
+        .addUserOption((option) =>
+          option.setName("user").setDescription("user to ping"),
+        ),
     )
-    .addUserOption((option) =>
-      option.setName("user").setDescription("user to ping"),
+    .addSubcommand((command) =>
+      command
+        .setName("mdn")
+        .setDescription("search MDN")
+        .addStringOption((option) =>
+          option
+            .setName("prompt")
+            .setDescription("term to search")
+            .setRequired(true),
+        )
+        .addUserOption((option) =>
+          option.setName("user").setDescription("user to ping"),
+        ),
     ),
   execute: async (interaction) => {
-    const userId = interaction.options.getUser("user")?.id;
-    const site = createSearchSite({
-      url: interaction.options.getString("site"),
-      prompt: interaction.options.getString("prompt"),
-    });
+    const site = searchSites[interaction.options.getSubcommand()];
+    const prompt = interaction.options.getString("prompt");
 
     const linkButton = new ButtonBuilder()
       .setEmoji("🔍")
-      .setLabel(site.description)
-      .setURL(site.searchURL)
+      .setLabel(`${site.name} results for "${prompt}"`)
+      .setURL(`${site.url}${prompt.replaceAll(" ", "+")}`)
       .setStyle(ButtonStyle.Link);
 
     const actionComponent = new ActionRowBuilder().setComponents(linkButton);
 
     await interaction.reply({
-      content: userId ? `<@${userId}>` : "",
       components: [actionComponent],
     });
   },
