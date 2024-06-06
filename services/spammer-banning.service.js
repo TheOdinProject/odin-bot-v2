e693636bc693560bdfbdd5394682d5e650a35a7d
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-06 11:36:57 +0200

    Reverse to send message directly

--- services/spammer-banning.service.js
+++ services/spammer-banning.service.js
@@ -3,33 +3,26 @@ const { EmbedBuilder } = require("discord.js");
 class SpammerBanningService {
   static async handleInteraction(interaction) {
     const message = interaction.options.getMessage("message");
-    console.log(message);
     if (message.author.bot) return;
-    const messageEmbed = await SpammerBanningService.#messageBuilder(message);
 
     try {
-      await SpammerBanningService.#sendMessageToUser(message.author, {
-        embeds: [messageEmbed],
-      });
+      await SpammerBanningService.#sendMessageToUser(message.author);
     } catch (error) {
       console.log(error);
     }
   }
 
-  static async #sendMessageToUser(user, message) {
-    await user.send(message);
-  }
-
-  static async #messageBuilder(message) {
-    return new EmbedBuilder()
+  static async #sendMessageToUser(user) {
+    const embed = new EmbedBuilder()
       .setTitle("Banned: Compromised account / Spam")
       .setDescription(
-        `
-      Hi <@${message.author.id}>, Your account has been banned because it is posting phishing links in our server.
+        `Account is compromised and is used to spam phishing links.
 
-If you would still like to continue using our server, make sure to change your password and recover your account.
-After that send a detailed contact information to appeal the ban on theodinprojectcontact@gmail.com`,
+  If you would still like to continue using our server, make sure to change your password and recover your account.
+  After that send a detailed contact information to appeal the ban on theodinprojectcontact@gmail.com`,
       );
+
+    await user.send(embed);
   }
 }
 
