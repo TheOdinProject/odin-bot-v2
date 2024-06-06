95642d4718d462a9af70d63c9af8c2930431862e
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-06 13:27:05 +0200

    Update tests and use snapshot

--- services/spam-ban/spam-banning.service.test.js
+++ services/spam-ban/spam-banning.service.test.js
@@ -2,24 +2,23 @@ const SpamBanningService = require("./spam-banning.service");
 
 describe("Successfully bans user", () => {
   // Message from interaction, will be changed per test
-  let interactionMessage;
+  const messageMock = { react: jest.fn(() => {}) };
   // Will be replaced by the reply coming from interaction
   let reply;
 
   const interactionMock = {
     options: {
-      getMessage: () => interactionMessage,
+      getMessage: () => messageMock,
     },
     reply: jest.fn((message) => {
       reply = message;
     }),
   };
   it("User is banned successfully on full ban", async () => {
-    const author = { id: "123", send: jest.fn(() => {}) };
-    const member = { ban: jest.fn(() => {}) };
-    interactionMessage = { author, member };
+    messageMock.author = { id: "123", send: jest.fn(() => {}) };
+    messageMock.member = { ban: jest.fn(() => {}) };
     await SpamBanningService.handleInteraction(interactionMock);
-    expect(author.send).toHaveBeenCalled();
+    expect(messageMock.author.send).toHaveBeenCalled();
     expect(reply).toMatchSnapshot();
   });
 });
