8624092a167fadf04a07b28f0cab40d75630d503
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-11 14:03:23 +0200

    Fix second test suite

--- services/spam-ban/spam-banning.service.test.js
+++ services/spam-ban/spam-banning.service.test.js
@@ -117,73 +117,53 @@ describe("Banning spammer with DM enabled", () => {
     expect(interactionMock.getReplyArg()).toMatchSnapshot();
   });
 });
-// describe("Banning spammer who has DM set to private", () => {
-//   let banArg;
-//   let reactArg;
-//
-//   const messageMock = {
-//     author: {
-//       id: "123",
-//       send: jest.fn(() => {
-//         throw new Error("Cannot send DM to user");
-//       }),
-//     },
-//     member: {
-//       ban: jest.fn((arg) => {
-//         banArg = arg;
-//       }),
-//     },
-//     react: jest.fn((arg) => {
-//       reactArg = arg;
-//     }),
-//   };
-//
-//   let reply;
-//   const interactionMock = {
-//     reply: jest.fn((message) => {
-//       reply = message;
-//     }),
-//     options: {
-//       getMessage: () => messageMock,
-//     },
-//     guild: createGuild(),
-//   };
-//
-//   it("Discord ban api is called with the correct reason", async () => {
-//     await SpamBanningService.handleInteraction(interactionMock);
-//     expect(messageMock.member.ban).toHaveBeenCalledTimes(1);
-//     expect(banArg).toMatchSnapshot();
-//   });
-//
-//   it("Discord message api is called and error handled", async () => {
-//     await SpamBanningService.handleInteraction(interactionMock);
-//     expect(messageMock.author.send).toHaveBeenCalledTimes(1);
-//   });
-//
-//   it("Reacts with the correct emoji", async () => {
-//     await SpamBanningService.handleInteraction(interactionMock);
-//     expect(messageMock.react).toHaveBeenCalledTimes(1);
-//     expect(reactArg).toMatchSnapshot();
-//   });
-//
-//   it("Sends log to the correct channel", async () => {
-//     await SpamBanningService.handleInteraction(interactionMock);
-//     interactionMock.guild.channels.cache.forEach((channel) => {
-//       if (channel.id === config.channels.moderationLog) {
-//         expect(channel.send).toHaveBeenCalledTimes(1);
-//         expect(channel.arg).toMatchSnapshot();
-//       } else {
-//         expect(channel.send).not.toHaveBeenCalledTimes(1);
-//       }
-//     });
-//   });
-//
-//   it("Sends back correct interaction reply to calling moderator", async () => {
-//     await SpamBanningService.handleInteraction(interactionMock);
-//     expect(reply).toMatchSnapshot();
-//   });
-// });
-//
+
+describe("Banning spammer who has DM set to private", () => {
+  let interactionMock;
+  beforeEach(() => {
+    const guildMock = createGuildMock();
+    const messageMock = createMessageMock();
+    messageMock.author.send = jest.fn(() => {
+      throw new Error("Can't contact user");
+    });
+    interactionMock = createInteractionMock(messageMock, guildMock);
+  });
+
+  it("Discord ban api is called with the correct reason", async () => {
+    await SpamBanningService.handleInteraction(interactionMock);
+    expect(interactionMock.message.member.ban).toHaveBeenCalledTimes(1);
+    expect(interactionMock.getBanArg()).toMatchSnapshot();
+  });
+
+  it("Discord message api is called and error handled", async () => {
+    await SpamBanningService.handleInteraction(interactionMock);
+    expect(interactionMock.message.author.send).toHaveBeenCalledTimes(1);
+  });
+
+  it("Reacts with the correct emoji", async () => {
+    await SpamBanningService.handleInteraction(interactionMock);
+    expect(interactionMock.message.react).toHaveBeenCalledTimes(1);
+    expect(interactionMock.getReactArg()).toMatchSnapshot();
+  });
+
+  it("Sends log to the correct channel", async () => {
+    await SpamBanningService.handleInteraction(interactionMock);
+    interactionMock.guild.channels.cache.forEach((channel) => {
+      if (channel.id === config.channels.moderationLog) {
+        expect(channel.send).toHaveBeenCalledTimes(1);
+        expect(channel.getSendArg()).toMatchSnapshot();
+      } else {
+        expect(channel.send).not.toHaveBeenCalledTimes(1);
+      }
+    });
+  });
+
+  it("Sends back correct interaction reply to calling moderator", async () => {
+    await SpamBanningService.handleInteraction(interactionMock);
+    expect(interactionMock.getReplyArg()).toMatchSnapshot();
+  });
+});
+
 // describe("Banning spammer that has left the server", () => {
 //   let reactArg;
 //
