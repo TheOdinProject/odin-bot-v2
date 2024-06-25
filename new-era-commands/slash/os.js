cd91b8ba1a2d5f9a5331a7c471b96eca2c6fc2be
Asartea <76259120+Asartea@users.noreply.github.com>
2024-06-25 11:53:28 +0200

    Feat: Add list of reason's why TOP doesn't support Windows

--- new-era-commands/slash/os.js
+++ new-era-commands/slash/os.js
@@ -11,7 +11,14 @@ module.exports = {
     const osEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('Windows')
-      .setDescription('**The Odin Project does not support Windows or any other OS outside of our recommendations**. We are happy to assist with any questions about installing a VM, using WSL, or dual booting Linux. <https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options>')
+      .setDescription(
+        `**The Odin Project does not support Windows or any other OS outside of our recommendations**. We are happy to assist with any questions about installing a VM, using WSL, or dual booting Linux.
+
+        For more info on Windows, check out this exhaustive list on '[Why we do not support Windows](<https://github.com/TheOdinProject/blog/wiki/Why-We-Do-Not-Support-Windows>)'.
+
+        <https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options>
+        `
+      )
       .setURL('https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options');
 
     await interaction.reply({
