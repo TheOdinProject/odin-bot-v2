3a5809786508ed40d3f54ba462dd6982cea911f8
Asartea <76259120+Asartea@users.noreply.github.com>
2024-07-14 18:08:53 +0200

    Add isAdmin() util

---
+++ utils/is-admin.js
@@ -0,0 +1,9 @@
+const { roles } = require('../config');
+
+function isAdmin(member) {
+  return member.roles.cache.some((role) =>
+    roles.adminRolesName.includes(role.name),
+  );
+}
+
+module.exports = { isAdmin };
