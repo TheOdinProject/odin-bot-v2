b1678337bacb1d65493a75417d523b523b1f427d
Asartea <76259120+Asartea@users.noreply.github.com>
2024-08-06 22:17:24 +0200

    Update new-era-commands/slash/help.js
    Co-authored-by: MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>

--- new-era-commands/slash/help.js
+++ new-era-commands/slash/help.js
@@ -30,8 +30,7 @@ module.exports = {
         {
           name: 'Helpful bot commands',
           value:
-            `Type \` /
-            \` in the message box to see the full list of commands! Explore them in our bot channel - <#${channels.botSpamPlaygroundChannelId}>.`,
+            `Type \`/\` in the message box to see the full list of commands! Explore them in our bot channel - <#${channels.botSpamPlaygroundChannelId}>.`,
         },
         {
           name: "It's TOP o'Clock, 24/7/365",
