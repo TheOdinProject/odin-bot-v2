c9fe03cee3698157cef2b516e6f0d422ca252407
Tosh <128783853+ItsTosh@users.noreply.github.com>
2024-08-07 22:11:07 +1000

    /support: Fix: Format files (#431)
    Format support.js file based off Prettier rules

--- new-era-commands/slash/support.js
+++ new-era-commands/slash/support.js
@@ -1,5 +1,9 @@
 const {
-  SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder,
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
 } = require('discord.js');
 
 module.exports = {
@@ -18,13 +22,17 @@ module.exports = {
       .setStyle(ButtonStyle.Link)
       .setEmoji('1270559118368964710');
 
-    const buttons = new ActionRowBuilder()
-      .addComponents(learnMoreButton, donateNowButton);
+    const buttons = new ActionRowBuilder().addComponents(
+      learnMoreButton,
+      donateNowButton,
+    );
 
     const supportEmbed = new EmbedBuilder()
       .setColor('#cc9543')
       .setTitle('Support The Project')
-      .setDescription('From the very beginning, The Odin Project has been committed to providing a world-class and completely free coding curriculum for anyone and everyone eager to learn. **With your generous donations**, we can continue to inspire thousands of aspiring developers, irrespective of their background or financial status.');
+      .setDescription(
+        'From the very beginning, The Odin Project has been committed to providing a world-class and completely free coding curriculum for anyone and everyone eager to learn. **With your generous donations**, we can continue to inspire thousands of aspiring developers, irrespective of their background or financial status.',
+      );
 
     await interaction.reply({ embeds: [supportEmbed], components: [buttons] });
   },
