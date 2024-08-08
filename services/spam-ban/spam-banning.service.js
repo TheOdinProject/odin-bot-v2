f12cc803dd6d6dc8233f6d95f9413604eb2823a1
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-08 08:15:01 +0200

    Run prettier

--- services/spam-ban/spam-banning.service.js
+++ services/spam-ban/spam-banning.service.js
@@ -1,12 +1,12 @@
-const { EmbedBuilder } = require("discord.js");
-const config = require("../../config");
+const { EmbedBuilder } = require('discord.js');
+const config = require('../../config');
 
 class SpamBanningService {
   static async handleInteraction(interaction) {
-    const message = interaction.options.getMessage("message");
+    const message = interaction.options.getMessage('message');
     if (message.author.bot || SpamBanningService.#isAdmin(message.member)) {
       interaction.reply({
-        content: "You do not have the permission to ban this user",
+        content: 'You do not have the permission to ban this user',
         ephemeral: true,
       });
       return;
@@ -16,7 +16,7 @@ class SpamBanningService {
       let reply;
 
       if (!message.member) {
-        message.react("❌");
+        message.react('❌');
         reply = `Couldn't bann <@${message.author.id}>. User is not on the server.`;
       } else {
         reply = await SpamBanningService.#banUser(message);
@@ -38,22 +38,27 @@ class SpamBanningService {
       reply = `Banned <@${message.author.id}> for spam but wasn't able to contact the user.`;
     }
 
-    message.member.ban({ reason: "Account is compromised" });
-    message.react("✅");
+    message.member.ban({ reason: 'Account is compromised' });
+    message.react('✅');
     return reply;
   }
 
   static async #sendMessageToUser(author) {
     const embedMessage = new EmbedBuilder()
-      .setTitle("Banned: Compromised account / Spam")
+      .setTitle('Banned: Compromised account / Spam')
       .setDescription(
         `Your account has been banned from The Odin Project Discord server for sending spam. If this account is compromised, please follow the steps linked in [this Discord support article](https://support.discord.com/hc/en-us/articles/24160905919511-My-Discord-Account-was-Hacked-or-Compromised) to secure your account.
 
 Once your account is secure, you may appeal your ban by emailing \`moderation@theodinproject.com\` with your Discord username and that you are appealing your ban due to a compromised account.
 
-Note: It may take several days for our volunteer staff to take action on your appeal, and unbanning is not guaranteed.`);
+Note: It may take several days for our volunteer staff to take action on your appeal, and unbanning is not guaranteed.`,
+      );
 
-    await author.send({content: "Your account has been banned, Please enable the embed option if you cannot see the message bellow.", embeds: [embedMessage] });
+    await author.send({
+      content:
+        'Your account has been banned, Please enable the embed option if you cannot see the message bellow.',
+      embeds: [embedMessage],
+    });
   }
 
   static async #announceBan(interaction, message) {
@@ -76,17 +81,17 @@ Note: It may take several days for our volunteer staff to take action on your ap
       fields: [
         {
           value: `<@${message.author.id}>`,
-          name: "User",
+          name: 'User',
           inline: true,
         },
         {
           value: `<@${interaction.user.id}>`,
-          name: "Moderator",
+          name: 'Moderator',
           inline: true,
         },
         {
-          value: "Account is compromised and spamming phishing links.",
-          name: "Reason",
+          value: 'Account is compromised and spamming phishing links.',
+          name: 'Reason',
           inline: true,
         },
       ],
