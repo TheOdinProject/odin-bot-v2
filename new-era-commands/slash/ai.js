6823cdccc926156f90557fd4d6b0939df576edc6
JustWaveThings <justwavethings@proton.me>
2024-08-17 17:29:54 -0400

    Update slash command RE: AI generated code

--- new-era-commands/slash/ai.js
+++ new-era-commands/slash/ai.js
@@ -10,24 +10,27 @@ module.exports = {
 
     const aiEmbed = new EmbedBuilder()
       .setColor('#cc9543')
-      .setTitle('Generative AI and Learning Code')
-      .setDescription(
-        'Learners should be aware of how generative AI tools can impact their learning.',
-      )
-      .addFields(
-        [
-          {
-            name: 'Using AI while learning',
-            value: 'The Odin Project recommends that learners avoid using generative AI while learning. [You can learn more about this recommendation here.](https://www.theodinproject.com/lessons/foundations-motivation-and-mindset#a-note-on-ai-code-generation)',
-            inline: true,
-          },
-          {
-            name: 'Chat about AI',
-            value: 'There is a [thread for discussions about AI tools](https://discord.com/channels/505093832157691914/1050497328915697664), so please chat there about these types of tools.',
-            inline: true,
-          },
-        ],
-      );
+      .setTitle('Generative AI and Learning to Code')
+      .addFields([
+        {
+          name: 'AI Etiquette',
+          value:
+            'Do not ask for help debugging code created by or with the help of AI tools. Requests to verify or examine the accuracy of AI-generated content (including code) are not permitted.',
+          inline: true,
+        },
+        {
+          name: 'Using AI while learning',
+          value:
+            'For a more productive journey, The Odin Project recommends that learners  [avoid using generative AI while learning](https://www.theodinproject.com/lessons/foundations-motivation-and-mindset#a-note-on-ai-code-generation).',
+          inline: true,
+        },
+        {
+          name: 'Chat about AI',
+          value:
+            'There is a [thread for discussions about AI tools](https://discord.com/channels/505093832157691914/1050497328915697664), so please chat there about these types of tools.',
+          inline: true,
+        },
+      ]);
 
     await interaction.reply({
       content: userId ? `${userId}` : '',
