fb2b00900435cf8c9bc6351838758f1cc18046e7
Asartea <76259120+Asartea@users.noreply.github.com>
2024-09-26 18:40:16 +0200

    /triage: Prevent rotate/swap if queue shorter than 2 users (#610)
    Doesn't make sense to rotate/swap 0 or 1 user and will cause undefined errors unless early return.

--- services/rotations/rotation.service.js
+++ services/rotations/rotation.service.js
@@ -151,6 +151,20 @@ class RotationService {
   async handleInteraction(interaction) {
     const actionType = interaction.options.getSubcommand();
 
+    const currentQueue = await this.#getQueue();
+    const currentQueueLength = currentQueue?.length || 0;
+
+    if (
+      currentQueueLength < 2 &&
+      actionType !== 'add' &&
+      actionType !== 'remove'
+    ) {
+      await interaction.reply(
+        'Less than two members in the queue. Try adding some with `/triage add`!',
+      );
+      return;
+    }
+
     const members = this.#getMembers(interaction.options);
 
     switch (actionType) {
