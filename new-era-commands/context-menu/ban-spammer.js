c6e0692e2f4e4559516933dfc97952b7a1763e59
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-12-01 03:48:44 +0100

    Format for linting

--- new-era-commands/context-menu/ban-spammer.js
+++ new-era-commands/context-menu/ban-spammer.js
@@ -1,9 +1,13 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType, PermissionFlagsBits, } = require("discord.js");
-const SpamBanningService = require("../../services/spam-ban/spam-banning.service");
+const {
+  ContextMenuCommandBuilder,
+  ApplicationCommandType,
+  PermissionFlagsBits,
+} = require('discord.js');
+const SpamBanningService = require('../../services/spam-ban/spam-banning.service');
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
-    .setName("Ban Spammer")
+    .setName('Ban Spammer')
     .setType(ApplicationCommandType.Message)
     .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
   execute: async (interaction) => {
