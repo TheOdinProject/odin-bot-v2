c8723acb02a9c96e20e21c8959a37add4647b85e
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-01-02 23:30:57 +0000

    Rephrase sentence

--- new-era-commands/slash/stuck.js
+++ new-era-commands/slash/stuck.js
@@ -16,7 +16,7 @@ Have a look through the following flowchart (click to enlarge) for guidance in h
 
 if you would like to ask for help, there is a very good question template you can find via the \`/question\` command which can help you include as much relevant context and structure your question in a way that makes it much easier for others to assist you.
 
-Make sure you've checked out our [guide to helping yourself before asking for help](<https://www.theodinproject.com/guides/community/before_asking>) to make sure you've done as much as you can first.
+Make sure you've checked out our [guide to helping yourself before asking for help](<https://www.theodinproject.com/guides/community/before_asking>) to ensure you've done as much as you can first.
       `)
       .setImage(
         'https://cdn.statically.io/gh/TheOdinProject/curriculum/978ba0e91a6a47452de819246f4c495e6b092f54/foundations/introduction/motivation_and_mindset/imgs/00.png'
