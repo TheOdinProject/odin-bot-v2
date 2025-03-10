2dfee0ff8e6cd749dab8389d109674387d4ebb4f
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-03-10 16:17:11 +0000

    Tweak startup error messages

--- utils/errors/missing-env-var.js
+++ utils/errors/missing-env-var.js
@@ -1,11 +1,11 @@
 class MissingEnvVarError extends Error {
   constructor(missingEnvVars) {
     super(`
-Could not start the bot. The following values are missing from .env:
+Could not start Odin Bot locally. The following value(s) are missing from your local .env file:
 
 ${missingEnvVars.join('\n')}
 
-Follow our "Getting Started" guide to obtain and set these values: https://github.com/TheOdinProject/odin-bot-v2/wiki/Getting-Started.
+Follow the Odin Bot "Getting Started" guide for instructions on how to obtain and set these values: https://github.com/TheOdinProject/odin-bot-v2/wiki/Getting-Started.
     `);
     this.name = 'MissingEnvVarError';
   }
--- utils/errors/no-redis.js
+++ utils/errors/no-redis.js
@@ -1,7 +1,7 @@
 class RedisConnectionError extends Error {
   constructor() {
     super(`
-Could not start the bot. Has Redis been installed and have you enabled the service?
+Could not start Odin Bot locally. Has Redis been installed and have you started/enabled the service?
 
 You can find your OS's Redis installation instructions and service start command at: https://redis.io/docs/latest/operate/oss_and_stack/install/install-redis/.
     `);
