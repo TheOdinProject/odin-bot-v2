18f9503749c7131d96ee12dd50ea0712542237a0
Asartea <76259120+Asartea@users.noreply.github.com>
2025-03-10 22:31:24 +0100

    Feat: Ensure Channel ID's are unique (#690)

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const { globSync } = require('glob');
 const path = require('path');
-const { token } = require('./config');
+const { token, channels } = require('./config');
 const events = require('./events');
 const MissingEnvVarError = require('./utils/errors/missing-env-var');
+const DuplicateIdsError = require('./utils/errors/duplicate-ids');
 
+// check all mandatory env variables are set
 const missingMandatoryEnvKeys = [
   'DISCORD_API_KEY',
   'DISCORD_CLIENT_ID',
@@ -13,6 +15,12 @@ const missingMandatoryEnvKeys = [
 ].filter((key) => !process.env[key]);
 if (missingMandatoryEnvKeys.length) {
   throw new MissingEnvVarError(missingMandatoryEnvKeys);
+}
+
+// check all channel IDs are unique
+const duplicateKeys = DuplicateIdsError.checkForDuplicateIds(channels);
+if (duplicateKeys.length) {
+  throw new DuplicateIdsError(duplicateKeys);
 }
 
 require('./bin/deploy-commands');
