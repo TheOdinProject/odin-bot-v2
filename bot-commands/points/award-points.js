6b607968e75b5d859f71df1163d20cb23b0ea9f9
mao-sz <122839503+mao-sz@users.noreply.github.com>
2025-09-25 18:23:42 +0100

    Points regex: Loosen pattern matching to reduce need for duplicate pings (#731)
    * Amend points regex tests for looser pattern matching
    
    Allows leading non-backslash/non-backtick and trailing non-word chars (e.g.
    punctuation) and allows multiple spaces between the ping and points
    chars (++).
    
    * Amend awardPoints regex for looser pattern matching
    
    * Allow trailing alphanumerical only if using :star: to award points
    
    Allows reasonable text like `@odinbot ⭐thanks!`, since the
    pre-increment question issue is only relevant for ++

--- bot-commands/points/award-points.js
+++ bot-commands/points/award-points.js
@@ -89,19 +89,31 @@ function plural(points) {
 }
 
 const userRegex = '<@!?(\\d+)>';
+
+// Don't disallow word chars after :star: - this should be perfectly valid: "@odinbot ⭐thanks!"
 const starRegex = '\u{2b50}';
 // matches at least two plus signs
 const plusRegex = '(\\+){2,}';
 const doublePointsPlusRegex = '\\?(\\+){2,}';
+// Word chars disallowed after ++-based points chars to prevent awarding points if a user pings someone
+// to ask about pre-increment syntax, e.g. "hey @odinbot ++i increments then evaluates, right?"
+// but will still allow stuff like punctuation e.g. "thanks @odinbot ++!"
+const plusBasedRegex = `(${plusRegex}|${doublePointsPlusRegex})(?!\\w)`;
+
+// https://regexr.com/8gd0p to test this regex
+//
+// Ensure the user mention is not escaped or encased directly in an inline code block
+// Not so simple to detect and prevent user mentions deeper within an inline or fenced code block though
+// But this is mostly prevented by Discord escaping user mentions when typing in them, so they won't match userRegex
+// Still technically possible by manually pasting something like <@!123456789> ++ in a code block (though it always was)
+const fullAwardPointsRegex = new RegExp(
+  `(?<!\\\\|\`)${userRegex}\\s*(${plusBasedRegex}|${starRegex})`,
+  'gu',
+);
 
 const awardPoints = {
   name: 'award points',
-  // uses a negative lookback to isolate the command
-  // followed by the Discord User, a whitespace character and either the star or plus incrementer
-  regex: new RegExp(
-    `(?<!\\S)${userRegex}\\s?(${doublePointsPlusRegex}|${plusRegex}|${starRegex})(?!\\S)`,
-    'gu',
-  ),
+  regex: fullAwardPointsRegex,
   cb: async function pointsBotCommand({
     author,
     content,
@@ -112,10 +124,7 @@ const awardPoints = {
   }) {
     const userIds = getUserIdsFromMessage(
       content,
-      new RegExp(
-        `(?<!\\S)${userRegex}\\s?(${doublePointsPlusRegex}|${plusRegex}|${starRegex})(?!\\S)`,
-        'gu',
-      ),
+      fullAwardPointsRegex,
       member,
       channel,
     );
