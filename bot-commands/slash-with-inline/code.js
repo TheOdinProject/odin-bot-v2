d41c5d49512da8791bdf4ac79115ebfd0cd30206
Ricardo N. <138608018+ric-projects@users.noreply.github.com>
2026-01-07 16:21:33 -0800

    Question templates: Add GitHub repo as an option for sharing code (#767)
    Affects /question, !question, /code, !code, and /data

--- bot-commands/slash-with-inline/code.js
+++ bot-commands/slash-with-inline/code.js
@@ -20,9 +20,10 @@ For \`inline code\` use one backtick (no syntax highlighting):
 
 **Websites:**
 
-- [Code Sandbox](https://codesandbox.io/) for Webpack/React projects
-- [Repl.it](https://replit.com/) for JavaScript/Ruby projects
 - [Codepen](https://codepen.io/) for basic HTML/CSS/Javascript
+- A link to an up-to-date GitHub repo
+- [Repl.it](https://replit.com/) for JavaScript/Ruby projects
+- [Code Sandbox](https://codesandbox.io/) for Webpack/React projects
 `);
 
 module.exports = {
