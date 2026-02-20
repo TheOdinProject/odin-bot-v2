89620b24f7fcc10368273d4eb0fa85ff515e97de
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-02-20 21:27:34 +0000

    User extracted discord mock features for isAdmin tests

--- utils/is-admin.test.js
+++ utils/is-admin.test.js
@@ -1,26 +1,17 @@
-const { Collection, Member } = require('discord.js');
+const { Collection } = require('discord.js');
+const { Member } = require('./mocks/discord').discordMock;
 const { isAdmin } = require('./is-admin');
-
-jest.mock('discord.js', () => ({
-  ...jest.requireActual('discord.js'),
-  Member: jest.fn().mockImplementation((roles) => ({
-    roles: {
-      cache: roles,
-    },
-  })),
-}));
 
 describe('isAdmin', () => {
   it('should return true if the user has an admin role', () => {
-    const memberCollection = new Collection();
-    memberCollection.set('role-1', { name: 'core' });
-    const member = Member(memberCollection);
+    const roles = new Collection().set('role-1', { name: 'core' });
+    const member = Member({ roles });
     expect(isAdmin(member)).toBe(true);
   });
 
   it('should return false if the user does not have an admin role', () => {
-    const memberCollection = new Collection();
-    const member = Member(memberCollection);
+    const roles = new Collection();
+    const member = Member({ roles });
     expect(isAdmin(member)).toBe(false);
   });
 });
