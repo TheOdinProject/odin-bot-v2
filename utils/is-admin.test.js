af9d409a3dbaf4fa6abb82484c86d213a1b38965
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-02-20 21:35:07 +0000

    Extends isAdmin tests for all expected admin roles

--- utils/is-admin.test.js
+++ utils/is-admin.test.js
@@ -3,13 +3,16 @@ const { Member } = require('./mocks/discord').discordMock;
 const { isAdmin } = require('./is-admin');
 
 describe('isAdmin', () => {
-  it('should return true if the user has an admin role', () => {
-    const roles = new Collection().set('role-1', { name: 'core' });
-    const member = Member({ roles });
-    expect(isAdmin(member)).toBe(true);
-  });
+  it.each([['core'], ['maintainer'], ['admin'], ['moderator']])(
+    'returns true if the user has the "%s" role',
+    (role) => {
+      const roles = new Collection().set('role-1', { name: role });
+      const member = Member({ roles });
+      expect(isAdmin(member)).toBe(true);
+    },
+  );
 
-  it('should return false if the user does not have an admin role', () => {
+  it('returns false if the user does not have an admin role', () => {
     const roles = new Collection();
     const member = Member({ roles });
     expect(isAdmin(member)).toBe(false);
