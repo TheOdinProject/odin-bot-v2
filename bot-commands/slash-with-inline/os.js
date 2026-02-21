8c9ae21ef2ece51237d206d12e576eb50b98466a
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-02-21 01:50:26 +0000

    /os: Update link to lesson about supported OSes (#795)
    Installations now combined with Installation Overview

--- bot-commands/slash-with-inline/os.js
+++ bot-commands/slash-with-inline/os.js
@@ -2,11 +2,11 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { color } = require('../../config');
 
 const osEmbed = new EmbedBuilder().setColor(color).setTitle('OS support')
-  .setDescription(`**The Odin Project is not designed for and does not support Windows or any other OS outside of our recommendations**. We are happy to assist with any questions about installing and using Ubuntu (or an official flavor) via a VM, WSL, or dual booting.
+  .setDescription(`**The Odin Project is not designed for and does not support Windows or any other OS outside of our recommendations**. We are happy to assist with any questions about installing and using Ubuntu (or an official flavor) via a VM, WSL2, or dual booting.
 
 For more info on Windows, check out this exhaustive list on [why we do not support Windows](<https://github.com/TheOdinProject/blog/wiki/Why-We-Do-Not-Support-Windows>).
 
-You can find our list of supported OS options in our [Installation Overview lesson](<https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/installation-overview#os-options>).`);
+You can find our list of supported OS options in our [Installations lesson](<https://www.theodinproject.com/lessons/foundations-installations#supported-operating-systems>).`);
 
 module.exports = {
   data: new SlashCommandBuilder()
