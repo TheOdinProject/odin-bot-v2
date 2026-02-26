840d7a016a5b53653b0b400e09e6f0adb9a809a9
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-02-26 22:51:56 +0000

    Use new class mocks for isAdmin tests

--- utils/is-admin.test.js
+++ utils/is-admin.test.js
@@ -1,20 +1,17 @@
-const { Collection } = require('discord.js');
-const { Member } = require('./mocks/discord');
+const { GuildMember } = require('./mocks/discord');
 const { isAdmin } = require('./is-admin');
 
 describe('isAdmin', () => {
   it.each([['core'], ['maintainer'], ['admin'], ['moderator']])(
     'returns true if the user has the "%s" role',
     (role) => {
-      const roles = new Collection().set('role-1', { name: role });
-      const member = Member({ roles });
+      const member = new GuildMember({ roles: [role] });
       expect(isAdmin(member)).toBe(true);
     },
   );
 
   it('returns false if the user does not have an admin role', () => {
-    const roles = new Collection();
-    const member = Member({ roles });
+    const member = new GuildMember({ roles: [] });
     expect(isAdmin(member)).toBe(false);
   });
 });
