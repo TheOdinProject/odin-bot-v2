b629d0345c60aba6264517680bf495f87cf0bbe2
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-03-13 00:11:15 +0000

    Add `/points info` to MongoDB version PointsService
    Unchanged from original

--- services/points/points-mongo.service.js
+++ services/points/points-mongo.service.js
@@ -13,7 +13,42 @@ class PointsService {
   static async handleInteraction(interaction) {
     if (interaction.options.getSubcommand() === 'user') {
       await PointsService.displayUserPoints(interaction);
+    } else {
+      await PointsService.displayInfo(interaction);
     }
+  }
+
+  static async displayInfo(interaction) {
+    const pointsEmbed = new EmbedBuilder()
+      .setColor('#cc9543')
+      .setTitle('Points in the TOP Discord server').setDescription(`
+Want to give credit where it's due? Show your appreciation for helpful members in our server by giving them a point! Mention their \`@name\` and add \`++\` or \`:star:\`
+
+**Example:**
+
+\`@username ++\`
+\`@username :star:\`
+
+The bot will only detect these in new messages, not message edits.
+
+**Club 40:**
+
+Users who have accumulated 40 points will be awarded a special role in recognition of their consistent helpfulness.
+For further details about our roles, please refer to [our discord #roles channel](https://discord.com/channels/505093832157691914/936424264180060200)
+
+**Point Inflation:**
+
+Please don't beg for points or abuse the point system.
+We will have a strict moderation policy in place which will include losing access to the bot or participation in the points system.
+
+Our goal is to maintain a positive and supportive community, where help and contributions are valued.
+      `);
+    const userId = interaction.options.getUser('user');
+
+    await interaction.reply({
+      content: userId ? `${userId}` : '',
+      embeds: [pointsEmbed],
+    });
   }
 
   static async displayUserPoints(interaction) {
