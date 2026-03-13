9cb00cbca1c4c5b71ef2ea1e34027fdedba726c2
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-03-13 21:09:48 +0000

    Put all points regex tests in same suite
    No need for separate deduct-points regex suite

--- bot-commands/points/points-regex.test.js
+++ bot-commands/points/points-regex.test.js
@@ -1,8 +1,17 @@
 const awardPoints = require('./award-points-mongo');
+const deductPoints = require('./deduct-points');
 
-describe('command', () => {
-  it('has the name "award points"', () => {
-    expect(awardPoints.name).toBe('award points');
+describe('commands', () => {
+  describe('award-points', () => {
+    it('has the name "award points"', () => {
+      expect(awardPoints.name).toBe('award points');
+    });
+  });
+
+  describe('deduce-points', () => {
+    it('has the name "deduct points"', () => {
+      expect(deductPoints.name).toBe('deduct points');
+    });
   });
 });
 
@@ -189,5 +198,49 @@ describe('regex', () => {
     it('does not match if the user mention is escaped', () => {
       expect('\\<@!123456789> ⭐'.match(awardPoints.regex)).toBeFalsy();
     });
+  });
+
+  describe('--', () => {
+    it.each([
+      ['<@!123456789> --'],
+      ['thanks <@!123456789> --'],
+      ['<@!123456789>--'],
+    ])('correct strings trigger the callback', (string) => {
+      expect(deductPoints.regex.test(string)).toBeTruthy();
+    });
+    it.each([
+      ['--'],
+      [''],
+      [' '],
+      [' /'],
+      ['odin-bot--'],
+      ['/--'],
+      ['```function("<@!123456789> --", () => {}```'],
+    ])("'%s' does not trigger the callback", (string) => {
+      expect(deductPoints.regex.test(string)).toBeFalsy();
+    });
+
+    it.each([
+      ['Check this out! <@!123456789> --'],
+      ["Don't worry about it <@!123456789> --"],
+      ['Hey <@!123456789> --'],
+      ['/ <@!123456789>-- ^ /me /leaderboard /tests$*'],
+    ])("'%s' - command can be anywhere in the string", (string) => {
+      expect(deductPoints.regex.test(string)).toBeTruthy();
+    });
+
+    it.each([
+      ['@user/--'],
+      ["it's about/<@!123456789>--"],
+      ['<@!123456789>--isanillusion'],
+      ['<@!123456789>--/'],
+      ['<@!123456789>--*'],
+      ['<@!123456789>--...'],
+    ])(
+      "'%s' - command should be its own word/group - no leading or trailing characters",
+      (string) => {
+        expect(deductPoints.regex.test(string)).toBeFalsy();
+      },
+    );
   });
 });
