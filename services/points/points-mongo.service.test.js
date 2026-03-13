e1ed5812706bb12ef4c6ab35c1cc42f314d3c37d
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-03-13 02:02:05 +0000

    Implement `/points leaderboard` with MongoDB

--- services/points/points-mongo.service.test.js
+++ services/points/points-mongo.service.test.js
@@ -124,3 +124,152 @@ describe('user', () => {
     });
   });
 });
+
+describe('leaderboard', () => {
+  const createInteraction = (integerOptions) => ({
+    guild: GUILD,
+    options: {
+      getSubcommand: () => 'leaderboard',
+      getInteger: (option) => integerOptions[option],
+    },
+    reply: jest.fn((message) => message),
+  });
+
+  it('Returns top 5 leaderboard when no options and limit set', async () => {
+    const interaction = createInteraction({});
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    expect(botReply.embeds[0].data).toMatchObject({
+      title: 'TOP Discord points leaderboard',
+      description: [
+        '1. User 30 - 30 :tada:',
+        '2. User 29 - 29',
+        '3. User 28 - 28',
+        '4. User 27 - 27',
+        '5. User 26 - 26',
+      ].join('\n'),
+    });
+  });
+
+  it('Returns LIMIT number of leaderboard entries', async () => {
+    const interaction = createInteraction({ limit: 7 });
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    expect(botReply.embeds[0].data).toMatchObject({
+      title: 'TOP Discord points leaderboard',
+      description: [
+        '1. User 30 - 30 :tada:',
+        '2. User 29 - 29',
+        '3. User 28 - 28',
+        '4. User 27 - 27',
+        '5. User 26 - 26',
+        '6. User 25 - 25',
+        '7. User 24 - 24',
+      ].join('\n'),
+    });
+  });
+
+  it('Caps LIMIT at 25 entries', async () => {
+    const interaction = createInteraction({ limit: 500 });
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    expect(botReply.embeds[0].data).toMatchObject({
+      title: 'TOP Discord points leaderboard',
+      description: [
+        '1. User 30 - 30 :tada:',
+        '2. User 29 - 29',
+        '3. User 28 - 28',
+        '4. User 27 - 27',
+        '5. User 26 - 26',
+        '6. User 25 - 25',
+        '7. User 24 - 24',
+        '8. User 23 - 23',
+        '9. User 22 - 22',
+        '10. User 21 - 21',
+        '11. User 20 - 20',
+        '12. User 19 - 19',
+        '13. User 18 - 18',
+        '14. User 17 - 17',
+        '15. User 16 - 16',
+        '16. User 15 - 15',
+        '17. User 14 - 14',
+        '18. User 13 - 13',
+        '19. User 12 - 12',
+        '20. User 11 - 11',
+        '21. User 10 - 10',
+        '22. User 9 - 9',
+        '23. User 8 - 8',
+        '24. User 7 - 7',
+        '25. User 6 - 6',
+      ].join('\n'),
+    });
+  });
+
+  it('Returns 5 leaderboard entries with a starting offset', async () => {
+    const interaction = createInteraction({ offset: 3 });
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    expect(botReply.embeds[0].data).toMatchObject({
+      title: 'TOP Discord points leaderboard',
+      description: [
+        '4. User 27 - 27',
+        '5. User 26 - 26',
+        '6. User 25 - 25',
+        '7. User 24 - 24',
+        '8. User 23 - 23',
+      ].join('\n'),
+    });
+  });
+
+  it('Cannot offset beyond leaderboard length', async () => {
+    const interaction = createInteraction({ offset: 500 });
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    expect(botReply.embeds[0].data).toMatchObject({
+      title: 'TOP Discord points leaderboard',
+      description: '30. User 1 - 1',
+    });
+  });
+
+  it('Allows both offset and limit options together', async () => {
+    const interaction = createInteraction({ limit: 6, offset: 5 });
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    expect(botReply.embeds[0].data).toMatchObject({
+      title: 'TOP Discord points leaderboard',
+      description: [
+        '6. User 25 - 25',
+        '7. User 24 - 24',
+        '8. User 23 - 23',
+        '9. User 22 - 22',
+        '10. User 21 - 21',
+        '11. User 20 - 20',
+      ].join('\n'),
+    });
+  });
+
+  it('Does not include non-guild members in leaderboard', async () => {
+    const interaction = createInteraction({});
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    const leaderboard = botReply.embeds[0]?.data.description;
+    expect(leaderboard).toEqual(expect.not.stringContaining('NotInGuild'));
+  });
+
+  it('Escaped markdown characters in response text', async () => {
+    const interaction = createInteraction({ offset: 5, limit: 25 });
+    await PointsService.handleInteraction(interaction);
+
+    const botReply = await interaction.reply.mock.results[0]?.value;
+    const leaderboard = botReply.embeds[0]?.data.description;
+    expect(leaderboard).toEqual(expect.stringContaining('\\*2\\*'));
+    expect(leaderboard).toEqual(expect.not.stringContaining('*2*'));
+  });
+});
