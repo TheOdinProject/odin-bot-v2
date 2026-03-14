3b39646775738e39684b6b0b7cfdbfd4e1626ee0
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-03-14 16:28:51 +0000

    Temporarily restrict new points channel send to non-prod envs
    Old points feature won't be removed immediately, so we don't want to
    double up on bot messages

--- bot-commands/points/award-points-mongo.js
+++ bot-commands/points/award-points-mongo.js
@@ -3,6 +3,8 @@ const club40Gifs = require('./club-40-gifs.json');
 const { isAdmin } = require('../../utils/is-admin');
 const PointsService = require('../../services/points/points-mongo.service');
 
+const temporarilyOnlyRunInDevOrTest = process.env.NODE_ENV !== 'production';
+
 function getRandomClub40Gif() {
   const choice = Math.floor(Math.random() * club40Gifs.length);
   return club40Gifs[choice];
@@ -75,9 +77,12 @@ const awardPoints = {
     guild,
     member: author,
   }) {
-    if (config.channels.noPointsChannelIds.includes(channel.id)) {
-      channel.send("You can't give points in this channel!");
-      return;
+    // TODO: Remove conditional when old points feature removed
+    if (temporarilyOnlyRunInDevOrTest) {
+      if (config.channels.noPointsChannelIds.includes(channel.id)) {
+        channel.send("You can't give points in this channel!");
+        return;
+      }
     }
 
     const awards = new Map();
@@ -89,29 +94,38 @@ const awardPoints = {
           awards.set(userID, 2);
         } else if (!hasNotifiedAboutDoublePoints) {
           hasNotifiedAboutDoublePoints = true;
-          channel.send('Only staff can use ?++ to give double points!');
+          // TODO: Remove conditional when old points feature removed
+          if (temporarilyOnlyRunInDevOrTest) {
+            channel.send('Only staff can use ?++ to give double points!');
+          }
         }
       } else if (awards.get(userID) !== 2) {
         awards.set(userID, 1);
       }
     }
 
-    const ODIN_BOT = client.user;
-    if (awards.has(ODIN_BOT.id)) {
-      channel.send('Awwwww shucks... :heart_eyes:');
-      awards.delete(ODIN_BOT.id);
-    }
-    if (awards.has(author.id)) {
-      channel.send('http://media0.giphy.com/media/RddAJiGxTPQFa/200.gif');
-      channel.send("You can't give yourself points!");
-      awards.delete(author.id);
+    // TODO: Remove conditional when old points feature removed
+    if (temporarilyOnlyRunInDevOrTest) {
+      const ODIN_BOT = client.user;
+      if (awards.has(ODIN_BOT.id)) {
+        channel.send('Awwwww shucks... :heart_eyes:');
+        awards.delete(ODIN_BOT.id);
+      }
+      if (awards.has(author.id)) {
+        channel.send('http://media0.giphy.com/media/RddAJiGxTPQFa/200.gif');
+        channel.send("You can't give yourself points!");
+        awards.delete(author.id);
+      }
     }
 
     const MAX_AWARDS_PER_MESSAGE = 5;
     if (awards.size > MAX_AWARDS_PER_MESSAGE) {
-      channel.send(
-        `You can only do up to ${MAX_AWARDS_PER_MESSAGE} users at a time...`,
-      );
+      // TODO: Remove conditional when old points feature removed
+      if (temporarilyOnlyRunInDevOrTest) {
+        channel.send(
+          `You can only do up to ${MAX_AWARDS_PER_MESSAGE} users at a time...`,
+        );
+      }
     }
 
     try {
@@ -126,37 +140,42 @@ const awardPoints = {
         );
       const updatedUsers = await Promise.all(databaseUpserts);
 
-      for (const { discordID, points } of updatedUsers) {
-        const awardedMember = guild.members.cache.get(discordID);
-        const isDoublePoints = awards.get(discordID) === 2;
-        channel.send(
-          `${exclamation(points, isDoublePoints)} ${awardedMember} now has ${points} ${getPlural(points)}`,
-        );
+      // TODO: Remove conditional when old points feature removed
+      if (temporarilyOnlyRunInDevOrTest) {
+        for (const { discordID, points } of updatedUsers) {
+          const awardedMember = guild.members.cache.get(discordID);
+          const isDoublePoints = awards.get(discordID) === 2;
+          channel.send(
+            `${exclamation(points, isDoublePoints)} ${awardedMember} now has ${points} ${getPlural(points)}`,
+          );
 
-        const isInClub40 = awardedMember.roles.cache.has(config.roles.club40Id);
-        if (points < 40 || isInClub40) {
-          continue;
+          const isInClub40 = awardedMember.roles.cache.has(
+            config.roles.club40Id,
+          );
+          if (points < 40 || isInClub40) {
+            continue;
+          }
+
+          const club40Channel = guild.channels.cache.get(
+            config.channels.club40ChannelId,
+          );
+          const club40Role = guild.roles.cache.get(config.roles.club40Id);
+          if (!club40Channel || !club40Role) {
+            throw new Error('No club 40 channel and/or role set!');
+          }
+
+          awardedMember.roles.add(club40Role);
+
+          const isNewToClub40 = points === (isDoublePoints ? 41 : 40);
+          const welcomeGif = getRandomClub40Gif();
+          const welcomeMessage = isNewToClub40
+            ? `HEYYY EVERYONE SAY HI TO ${awardedMember} the newest member of CLUB 40! Please check the pins at the top right!`
+            : `WELCOME BACK TO CLUB 40 ${awardedMember}!! Please review the pins at the top right!`;
+
+          club40Channel.send(welcomeMessage);
+          club40Channel.send(welcomeGif.gif);
+          club40Channel.send(`Gif by ${welcomeGif.author}`);
         }
-
-        const club40Channel = guild.channels.cache.get(
-          config.channels.club40ChannelId,
-        );
-        const club40Role = guild.roles.cache.get(config.roles.club40Id);
-        if (!club40Channel || !club40Role) {
-          throw new Error('No club 40 channel and/or role set!');
-        }
-
-        awardedMember.roles.add(club40Role);
-
-        const isNewToClub40 = points === (isDoublePoints ? 41 : 40);
-        const welcomeGif = getRandomClub40Gif();
-        const welcomeMessage = isNewToClub40
-          ? `HEYYY EVERYONE SAY HI TO ${awardedMember} the newest member of CLUB 40! Please check the pins at the top right!`
-          : `WELCOME BACK TO CLUB 40 ${awardedMember}!! Please review the pins at the top right!`;
-
-        club40Channel.send(welcomeMessage);
-        club40Channel.send(welcomeGif.gif);
-        club40Channel.send(`Gif by ${welcomeGif.author}`);
       }
     } catch (error) {
       console.log(error);
