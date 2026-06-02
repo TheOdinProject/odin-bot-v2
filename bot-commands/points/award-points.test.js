52bcb76392fbd69c045bfbd29464231c9593aeed
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-02 23:37:15 +0100

    Add tests for awarding double points (?++)

--- bot-commands/points/award-points.test.js
+++ bot-commands/points/award-points.test.js
@@ -2,6 +2,7 @@ const {
   Guild,
   GuildMember,
   TextChannel,
+  Role,
 } = require('../../utils/mocks/discord');
 const awardPoints = require('./award-points-pg');
 const config = require('../../config');
@@ -299,22 +300,20 @@ describe('++ / :star:', () => {
 });
 
 describe('?++', () => {
-  const author = new GuildMember({ id: '99999999' });
-  const generalChannel = new TextChannel('000');
-  const club40Channel = new TextChannel(config.channels.club40ChannelId);
-  const noPointsChannel = new TextChannel(
-    config.channels.noPointsChannelIds[0],
-  );
-  const channels = [generalChannel, club40Channel, noPointsChannel];
+  const nonStaffAuthor = new GuildMember({ id: '99999999' });
+  const staffAuthor = new GuildMember({
+    id: '0000000',
+    roles: [new Role(1, 'core')],
+  });
 
-  it('Awards point to different member without points', async () => {
+  it('Prevents awarding points if author is not staff member', async () => {
     const mentionedMember = new GuildMember({ id: '0' });
     await awardPoints.cb({
-      member: author,
-      content: `${mentionedMember} ++`,
+      member: nonStaffAuthor,
+      content: `${mentionedMember} ?++`,
       channel: generalChannel,
       guild: new Guild({
-        members: [author, mentionedMember],
+        members: [nonStaffAuthor, mentionedMember],
         channels,
       }),
     });
@@ -322,20 +321,20 @@ describe('?++', () => {
     const result = await db.query(
       "SELECT points FROM points WHERE discord_id = '0';",
     );
-    expect(result.rows).toEqual([{ points: 1 }]);
+    expect(result.rows).toEqual([]);
     expect(generalChannel.send.mock.calls).toEqual([
-      [`Nice! ${mentionedMember} now has 1 point`],
+      ['Only staff can use ?++ to give double points!'],
     ]);
   });
 
-  it('Awards single point to different member with points', async () => {
+  it('Awards double points to different member if author is staff member', async () => {
     const mentionedMember = new GuildMember({ id: '1' });
     await awardPoints.cb({
-      member: author,
-      content: `${mentionedMember} ++`,
+      member: staffAuthor,
+      content: `${mentionedMember} ?++`,
       channel: generalChannel,
       guild: new Guild({
-        members: [author, mentionedMember],
+        members: [staffAuthor, mentionedMember],
         channels,
       }),
     });
@@ -343,43 +342,43 @@ describe('?++', () => {
     const result = await db.query(
       "SELECT points FROM points WHERE discord_id = '1';",
     );
-    expect(result.rows).toEqual([{ points: 2 }]);
+    expect(result.rows).toEqual([{ points: 3 }]);
     expect(generalChannel.send.mock.calls).toEqual([
-      [`Nice! ${mentionedMember} now has 2 points`],
+      [`Thanks for the great question! ${mentionedMember} now has 3 points`],
     ]);
   });
 
-  it('Awards single point to each of multiple different mentioned members', async () => {
+  it('Awards double points to each of multiple different mentioned members', async () => {
     const mentionedMember1 = new GuildMember({ id: '1' });
     const mentionedMember2 = new GuildMember({ id: '2' });
     await awardPoints.cb({
-      member: author,
-      content: `${mentionedMember1} ++ ${mentionedMember2} ++`,
+      member: staffAuthor,
+      content: `${mentionedMember1} ?++ ${mentionedMember2} ?++`,
       channel: generalChannel,
       guild: new Guild({
-        members: [author, mentionedMember1, mentionedMember2],
+        members: [staffAuthor, mentionedMember1, mentionedMember2],
         channels,
       }),
     });
 
     const result = await db.query(
-      "SELECT points FROM points WHERE discord_id IN ('1', '2');",
+      "SELECT points FROM points WHERE discord_id IN('1', '2');",
     );
-    expect(result.rows).toEqual([{ points: 2 }, { points: 3 }]);
+    expect(result.rows).toEqual([{ points: 3 }, { points: 4 }]);
     expect(generalChannel.send.mock.calls).toEqual([
-      [`Nice! ${mentionedMember1} now has 2 points`],
-      [`Nice! ${mentionedMember2} now has 3 points`],
+      [`Thanks for the great question! ${mentionedMember1} now has 3 points`],
+      [`Thanks for the great question! ${mentionedMember2} now has 4 points`],
     ]);
   });
 
-  it('Awards point only once for member mentioned multiple times', async () => {
+  it('Awards points only once for member mentioned multiple times', async () => {
     const mentionedMember = new GuildMember({ id: '1' });
     await awardPoints.cb({
-      member: author,
-      content: `${mentionedMember} ++ ${mentionedMember} ++`,
+      member: staffAuthor,
+      content: `${mentionedMember} ?++ ${mentionedMember} ?++`,
       channel: generalChannel,
       guild: new Guild({
-        members: [author, mentionedMember],
+        members: [staffAuthor, mentionedMember],
         channels,
       }),
     });
@@ -387,61 +386,18 @@ describe('?++', () => {
     const result = await db.query(
       "SELECT points FROM points WHERE discord_id = '1';",
     );
-    expect(result.rows).toEqual([{ points: 2 }]);
+    expect(result.rows).toEqual([{ points: 3 }]);
     expect(generalChannel.send.mock.calls).toEqual([
-      [`Nice! ${mentionedMember} now has 2 points`],
-    ]);
-  });
-
-  it('Limits awards in a single message to 5', async () => {
-    const mentionedMembers = [
-      new GuildMember({ id: '1' }),
-      new GuildMember({ id: '2' }),
-      new GuildMember({ id: '3' }),
-      new GuildMember({ id: '4' }),
-      new GuildMember({ id: '5' }),
-      new GuildMember({ id: '6' }),
-    ];
-    await awardPoints.cb({
-      member: author,
-      content: `${mentionedMembers[0]} ++ ${mentionedMembers[1]} ++ ${mentionedMembers[2]} ++ ${mentionedMembers[3]} ++ ${mentionedMembers[4]} ++ ${mentionedMembers[5]} ++`,
-      channel: generalChannel,
-      guild: new Guild({
-        members: [author, ...mentionedMembers],
-        channels,
-      }),
-    });
-
-    const result = await db.query(
-      `
-      SELECT points FROM points
-      WHERE discord_id IN ('1', '2', '3', '4', '5', '6');
-      `,
-    );
-    expect(result.rows).toEqual([
-      { points: 2 },
-      { points: 3 },
-      { points: 4 },
-      { points: 5 },
-      { points: 6 },
-      { points: 6 }, // points unchanged!
-    ]);
-    expect(generalChannel.send.mock.calls).toEqual([
-      ['You can only do up to 5 users at a time...'],
-      [`Nice! ${mentionedMembers[0]} now has 2 points`],
-      [`Nice! ${mentionedMembers[1]} now has 3 points`],
-      [`Nice! ${mentionedMembers[2]} now has 4 points`],
-      [`Sweet! ${mentionedMembers[3]} now has 5 points`],
-      [`Sweet! ${mentionedMembers[4]} now has 6 points`],
+      [`Thanks for the great question! ${mentionedMember} now has 3 points`],
     ]);
   });
 
   it('Gives unique response when trying to award OdinBot', async () => {
     await awardPoints.cb({
-      member: author,
-      content: `${GuildMember.odinBot} ++`,
+      member: staffAuthor,
+      content: `${GuildMember.odinBot} ?++`,
       channel: generalChannel,
-      guild: new Guild({ members: [author], channels }),
+      guild: new Guild({ members: [staffAuthor], channels }),
     });
 
     const result = await db.query(
@@ -457,11 +413,11 @@ describe('?++', () => {
   it('Prevents awarding points in a no-points channel', async () => {
     const mentionedMember = new GuildMember({ id: '0' });
     await awardPoints.cb({
-      member: author,
-      content: `${mentionedMember} ++`,
+      member: staffAuthor,
+      content: `${mentionedMember} ?++`,
       channel: noPointsChannel,
       guild: new Guild({
-        members: [author, mentionedMember],
+        members: [staffAuthor, mentionedMember],
         channels,
       }),
     });
@@ -477,15 +433,15 @@ describe('?++', () => {
 
   it('Prevents self-awarding points', async () => {
     await awardPoints.cb({
-      member: author,
-      content: `${author} ++`,
+      member: staffAuthor,
+      content: `${staffAuthor} ?++`,
       channel: generalChannel,
-      guild: new Guild({ members: [author], channels }),
+      guild: new Guild({ members: [staffAuthor], channels }),
     });
 
     const result = await db.query(
       'SELECT points FROM points WHERE discord_id = $1;',
-      [author.id],
+      [staffAuthor.id],
     );
     expect(result.rows).toEqual([]);
     expect(generalChannel.send.mock.calls).toEqual([
@@ -497,69 +453,61 @@ describe('?++', () => {
   it('Does not prevent awarding points to other members when also self-awarding', async () => {
     const mentionedMember = new GuildMember({ id: '0' });
     await awardPoints.cb({
-      member: author,
-      content: `${author} ++ ${mentionedMember} ++`,
+      member: staffAuthor,
+      content: `${staffAuthor} ?++ ${mentionedMember} ?++`,
       channel: generalChannel,
-      guild: new Guild({ members: [author, mentionedMember], channels }),
+      guild: new Guild({ members: [staffAuthor, mentionedMember], channels }),
     });
 
-    const ownAwardResult = await db.query(
-      'SELECT points FROM points WHERE discord_id = $1;',
-      [author.id],
-    );
-    const otherAwardResult = await db.query(
+    const result = await db.query(
       "SELECT points FROM points WHERE discord_id = '0';",
     );
-
-    expect(ownAwardResult.rows).toEqual([]);
-    expect(otherAwardResult.rows).toEqual([{ points: 1 }]);
+    expect(result.rows).toEqual([{ points: 2 }]);
     expect(generalChannel.send.mock.calls).toEqual([
       [selfAwardGif],
       ["You can't give yourself points!"],
-      [`Nice! ${mentionedMember} now has 1 point`],
+      [`Thanks for the great question! ${mentionedMember} now has 2 points`],
     ]);
   });
 
-  it('Does not include self-awards in 5 award limit', async () => {
-    const mentionedMembers = [
-      new GuildMember({ id: '1' }),
-      new GuildMember({ id: '2' }),
-      new GuildMember({ id: '3' }),
-      new GuildMember({ id: '4' }),
-      new GuildMember({ id: '5' }),
-    ];
+  it('Only uses the ?++ exclamation for ?++ awards (inc. mixed awards)', async () => {
+    const mentionedMember1 = new GuildMember({ id: '1' });
+    const mentionedMember2 = new GuildMember({ id: '2' });
     await awardPoints.cb({
-      member: author,
-      content: `${author} ++ ${mentionedMembers[0]} ++ ${mentionedMembers[1]} ++ ${mentionedMembers[2]} ++ ${mentionedMembers[3]} ++ ${mentionedMembers[4]} ++ ${mentionedMembers[5]} ++`,
+      member: staffAuthor,
+      content: `${mentionedMember1} ?++ ${mentionedMember2} ++`,
       channel: generalChannel,
       guild: new Guild({
-        members: [author, ...mentionedMembers],
+        members: [staffAuthor, mentionedMember1, mentionedMember2],
         channels,
       }),
     });
 
     const result = await db.query(
-      `
-      SELECT points FROM points
-      WHERE discord_id IN ($1, '1', '2', '3', '4', '5');
-      `,
-      [author.id],
+      "SELECT points FROM points WHERE discord_id IN ('1', '2');",
     );
-    expect(result.rows).toEqual([
-      { points: 2 },
-      { points: 3 },
-      { points: 4 },
-      { points: 5 },
-      { points: 6 },
-    ]);
+    expect(result.rows).toEqual([{ points: 3 }, { points: 3 }]);
     expect(generalChannel.send.mock.calls).toEqual([
-      [selfAwardGif],
-      ["You can't give yourself points!"],
-      [`Nice! ${mentionedMembers[0]} now has 2 points`],
-      [`Nice! ${mentionedMembers[1]} now has 3 points`],
-      [`Nice! ${mentionedMembers[2]} now has 4 points`],
-      [`Sweet! ${mentionedMembers[3]} now has 5 points`],
-      [`Sweet! ${mentionedMembers[4]} now has 6 points`],
+      [`Thanks for the great question! ${mentionedMember1} now has 3 points`],
+      [`Nice! ${mentionedMember2} now has 3 points`],
+    ]);
+  });
+
+  it('Prioritizes double points if same member given mixed awards in the same message', async () => {
+    const mentionedMember = new GuildMember({ id: '0' });
+    await awardPoints.cb({
+      member: staffAuthor,
+      content: `${mentionedMember} ?++ ${mentionedMember} ?++`,
+      channel: generalChannel,
+      guild: new Guild({ members: [staffAuthor, mentionedMember], channels }),
+    });
+
+    const result = await db.query(
+      "SELECT points FROM points WHERE discord_id = '0';",
+    );
+    expect(result.rows).toEqual([{ points: 2 }]);
+    expect(generalChannel.send.mock.calls).toEqual([
+      [`Thanks for the great question! ${mentionedMember} now has 2 points`],
     ]);
   });
 });
