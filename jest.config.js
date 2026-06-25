fc5939ad77680abaa34fdbb3b2a8feba346f5eb0
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-25 17:06:44 +0100

    Drop all tables in test DB after test run
    Ensures no lingering data can interfere with a future test run's setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   globalSetup: './test/global-setup.js',
+  globalTeardown: './test/global-teardown.js',
   testEnvironment: 'node',
   transformIgnorePatterns: ['/node_modules/(?!axios)'],
 };
