6875f45605423c958396398b5a71731e0edff7f6
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-27 14:30:17 +0100

    Use migration for points table creation instead
    Less brittle when we add or migrate other bot features to PostgreSQL,
    e.g. current Redis stuff

--- db/index.js
+++ db/index.js
@@ -1,16 +1,6 @@
 const { Pool } = require('pg');
 const config = require('../config');
 
-const pool = new Pool({
-  connectionString: config.databaseUrl,
-  onConnect: async (client) => {
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS points (
-        discord_id   text PRIMARY KEY,
-        points       integer NOT NULL
-      );
-    `);
-  },
-});
+const pool = new Pool({ connectionString: config.databaseUrl });
 
 module.exports = pool;
---
+++ db/schema.sql
@@ -0,0 +1,69 @@
+\restrict dbmate
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: points; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.points (
+    discord_id text NOT NULL,
+    points integer NOT NULL
+);
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+--
+-- Name: points points_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.points
+    ADD CONSTRAINT points_pkey PRIMARY KEY (discord_id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict dbmate
+
+
+--
+-- Dbmate schema migrations
+--
+
+INSERT INTO public.schema_migrations (version) VALUES
+    ('20260627132736');
