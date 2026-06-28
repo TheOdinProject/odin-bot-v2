481133f2c76f054514c06916c23980b3c4ad2d1e
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-28 15:17:27 +0100

    Fix SSL check
    As per Heroku docs: https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-node-js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 const config = require('../config');
 
-const pool = new Pool({ connectionString: config.databaseUrl });
+const pool = new Pool({
+  connectionString: config.databaseUrl,
+  ssl: { rejectUnauthorized: false },
+});
 
 module.exports = pool;
