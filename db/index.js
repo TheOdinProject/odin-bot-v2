7eb196541af66d19caf24b3181de065bf7bc1634
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-29 02:56:10 +0100

    Chore: Comment Heroku Postgres Node.js doc link in db pool (#864)

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ const config = require('../config');
 
 const pool = new Pool({
   connectionString: config.databaseUrl,
+  // as per https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-node-js
   ssl: { rejectUnauthorized: false },
 });
 
