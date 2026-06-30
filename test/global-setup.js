ad008b8921040f96dcc793a87800f488d5b2b57c
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-30 07:43:00 +0100

    Jest: Fix DB pool/migration issues in watch mode (#872)

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,9 +1,15 @@
 require('dotenv').config({ quiet: true });
 const MissingEnvVarError = require('../utils/errors/missing-env-var');
+const { execSync } = require('node:child_process');
 
 module.exports = () => {
   const missingMandatoryEnvKeys = MissingEnvVarError.getMissingMandatoryKeys();
   if (missingMandatoryEnvKeys.length) {
     throw new MissingEnvVarError(missingMandatoryEnvKeys);
   }
+
+  // Must run DB migrations in global setup and not in package.json script
+  // to ensure if watch mode is used, the migrations apply on each test rerun,
+  // not just the first run (global teardown resets the DB).
+  execSync('npx dbmate --env=TEST_DATABASE_URL --no-dump-schema up');
 };
