5304e85e077b5880d44cf006901fbac384549eea
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-30 07:20:46 +0100

    Expand DatabaseError messages
    More than one potential database error - ensure error message is
    relevant

--- utils/errors/database.js
+++ utils/errors/database.js
@@ -1,19 +1,39 @@
 const { execSync } = require('node:child_process');
 
 class DatabaseError extends Error {
-  constructor() {
-    super(
+  static #databaseEnvVar =
+    process.env.NODE_ENV === 'test' ? 'TEST_DATABASE_URL' : 'DATABASE_URL';
+  static #messages = new Map()
+    .set(
+      1,
       'There are migrations pending. Please run `npm run migrate` to apply these migrations.',
+    )
+    .set(
+      2,
+      `
+Unable to connect to the database.
+Is the ${DatabaseError.#databaseEnvVar} connection string correct?
+Have you made sure to enable and start the PostgreSQL service on your system?
+      `,
     );
+
+  constructor(errorCode) {
+    const message =
+      DatabaseError.#messages.get(errorCode) ??
+      `Unknown error code ${errorCode} when trying to connect to the database. Please file a bug report: https://github.com/TheOdinProject/odin-bot-v2/issues/new?template=bug_report.yaml`;
+
+    super(message);
     this.name = 'DatabaseError';
   }
 
-  static hasPendingMigrations() {
+  static checkMigrations() {
     try {
-      execSync('npm run migrate:status');
-      return false;
-    } catch {
-      return true;
+      execSync(
+        `npx dbmate --env=${DatabaseError.#databaseEnvVar} status --exit-code`,
+      );
+      return 0;
+    } catch (error) {
+      return error.status;
     }
   }
 }
