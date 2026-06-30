c430d369882857dff5cfcb02b4e8215826593fef
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-30 08:00:54 +0100

    Throw DatabaseError in Jest global startup if connection error

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,11 +1,23 @@
 require('../utils/load-env').load();
 const MissingEnvVarError = require('../utils/errors/missing-env-var');
+const DatabaseError = require('../utils/errors/database');
 const { execSync } = require('node:child_process');
+
+const PENDING_MIGRATIONS_ERROR_CODE = 1;
 
 module.exports = () => {
   const missingMandatoryEnvKeys = MissingEnvVarError.getMissingMandatoryKeys();
   if (missingMandatoryEnvKeys.length) {
     throw new MissingEnvVarError(missingMandatoryEnvKeys);
+  }
+
+  const databaseErrorCode = DatabaseError.checkMigrations();
+  // no need to throw if pending migrations, automatically applied after this
+  if (
+    databaseErrorCode &&
+    databaseErrorCode !== PENDING_MIGRATIONS_ERROR_CODE
+  ) {
+    throw new DatabaseError(databaseErrorCode);
   }
 
   // Must run DB migrations in global setup and not in package.json script
