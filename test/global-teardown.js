0651f5f31b961fb5c581970d255af12ea389d26e
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-30 07:33:41 +0100

    Fix DB pool/migration issues in Jest watch mode

--- test/global-teardown.js
+++ test/global-teardown.js
@@ -1,10 +1,17 @@
 const db = require('../db');
 
-module.exports = async () => {
-  // ensure no test run can interfere with the setup of a future test run
+module.exports = async ({ watch, watchAll }) => {
+  // Ensure no test run can interfere with the setup of a future test run
   await db.query(`
     DROP SCHEMA public CASCADE;
     CREATE SCHEMA public;
   `);
-  await db.end();
+
+  // Pool instance shared between watch-mode runs, so ending it in teardown interferes with reruns.
+  // In watch mode, you manually interrupt the process anyway
+  // so calling `.end()` only necessary in non-watch test runs where Jest is what exits the process
+  const isWatchMode = watch || watchAll;
+  if (!isWatchMode) {
+    await db.end();
+  }
 };
