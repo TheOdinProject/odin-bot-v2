bfb5eb622588bc42d6fde4b52d07db0f63cbfb10
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-08-16 18:56:42 +0100

    Move DB error code definitions to DatabaseError class

--- utils/errors/database.js
+++ utils/errors/database.js
@@ -3,13 +3,17 @@ const { execSync } = require('node:child_process');
 class DatabaseError extends Error {
   static #databaseEnvVar =
     process.env.NODE_ENV === 'test' ? 'TEST_DATABASE_URL' : 'DATABASE_URL';
+  static #codes = {
+    pendingMigrations: 1,
+    connectionError: 2,
+  };
   static #messages = new Map()
     .set(
-      1,
+      DatabaseError.#codes.pendingMigrations,
       'There are migrations pending. Please run `npm run migrate` to apply these migrations.',
     )
     .set(
-      2,
+      DatabaseError.#codes.connectionError,
       `
 Unable to connect to the database.
 Is the ${DatabaseError.#databaseEnvVar} connection string correct?
@@ -24,6 +28,10 @@ Have you made sure to enable and start the PostgreSQL service on your system?
 
     super(message);
     this.name = 'DatabaseError';
+  }
+
+  static get codes() {
+    return DatabaseError.#codes;
   }
 
   static checkMigrations() {
